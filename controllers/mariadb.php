d48b7cb88bb77501272cbf7d55faf17652c38b8a
pcbaldwin <pbaldwin@clearfoundation.com>
2014-12-23 23:50:07 -0500

    First commit

---
+++ controllers/mariadb.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * MariaDB controller.
+ *
+ * @category   apps
+ * @package    mariadb
+ * @subpackage controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2014 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/mariadb/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * MariaDB controller.
+ *
+ * @category   apps
+ * @package    mariadb
+ * @subpackage controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2014 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/mariadb/
+ */
+
+class MariaDB extends ClearOS_Controller
+{
+    /**
+     * MariaDB default controller
+     *
+     * @return view
+     */
+
+    function index()
+    {
+        // Load libraries
+        //---------------
+
+        $this->lang->load('mariadb');
+
+        // Load views
+        //-----------
+
+        $views = array(
+            'mariadb/server',
+            'mariadb/setting'
+        );
+
+        $this->page->view_forms($views, lang('mariadb_mariadb_database'));
+    }
+}
---
+++ deploy/info.php
@@ -0,0 +1,46 @@
+<?php
+
+/////////////////////////////////////////////////////////////////////////////
+// General information
+/////////////////////////////////////////////////////////////////////////////
+
+$app['basename'] = 'mariadb';
+$app['version'] = '2.0.0';
+$app['release'] = '1';
+$app['vendor'] = 'ClearFoundation';
+$app['packager'] = 'ClearFoundation';
+$app['license'] = 'GPLv3';
+$app['license_core'] = 'LGPLv3';
+$app['description'] = lang('mariadb_app_description');
+$app['tooltip'] = lang('mariadb_app_tooltip');
+
+/////////////////////////////////////////////////////////////////////////////
+// App name and categories
+/////////////////////////////////////////////////////////////////////////////
+
+$app['name'] = lang('mariadb_app_name');
+$app['category'] = lang('base_category_server');
+$app['subcategory'] = lang('base_subcategory_database');
+
+/////////////////////////////////////////////////////////////////////////////
+// Packaging
+/////////////////////////////////////////////////////////////////////////////
+
+$app['core_requires'] = array(
+    'app-base-core >= 1:1.2.6',
+    'app-network-core', 
+    'app-storage-core >= 1:1.4.7',
+    'mariadb-server >= 5.5.40',
+    'phpMyAdmin >= 4.1.13'
+);
+
+$app['core_file_manifest'] = array( 
+    'mariadb_default.conf' => array ( 'target' => '/etc/clearos/storage.d/mariadb_default.conf' ),
+    'mariadb.php'=> array( 'target' => '/var/clearos/base/daemon/mariadb.php' ),
+);
+
+$app['delete_dependency'] = array(
+    'app-mariadb-core',
+    'mariadb-server',
+    'phpMyAdmin'
+);
