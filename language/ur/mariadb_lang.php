119b75404e4c666016b5db9f82349048dcf8d95e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:30:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB ڈیٹا بیس سرور';
+$lang['mariadb_app_description'] = 'MariaDB ایک مقبول ڈیٹا بیس کا نظام ہے. اس ڈیٹا بیس پر مبنی ایپلی کیشنز، ویب سائٹس، CRM اور عملی طور پر کسی بھی دوسرے وسائل ایک سنبندپرک سٹوریج سروس کی ضرورت ہوتی ہے کو چلانے کے لئے تشکیل کر سکتے ہیں.';
+$lang['mariadb_app_tooltip'] = 'ڈیٹا بیس کی طرف سے استعمال جڑ اکاؤنٹ کے نظام کی طرف سے استعمال ایک سے مختلف ہے!';
+$lang['mariadb_mariadb_database'] = 'MariaDB ڈیٹا بیس';
+$lang['mariadb_access_denied'] = 'رسائی منع کی جاتی ہے.';
+$lang['mariadb_password_updated'] = 'پاس ورڈ کامیابی سے اپ ڈیٹ.';
+$lang['mariadb_username_invalid'] = 'صارف کا نام غلط ہے.';
+$lang['mariadb_hostname_invalid'] = 'میزبان نام غلط ہے.';
+$lang['mariadb_password_invalid'] = 'پاس ورڈ غلط ہے.';
+$lang['mariadb_password_mismatch'] = 'پاس ورڈ کی توثیق کرنے اور مماثل نہیں ہیں.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ایک ڈیٹا بیس پاس ورڈ مقرر کریں.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB ذخیرہ';
+$lang['mariadb_management_tool'] = 'MariaDB انتظام آلے';
+$lang['mariadb_management_tool_help'] = 'MariaDB انتظام کے آلے تک رسائی حاصل کرنے لنک پر عمل کریں. آپ کو آپ کے ڈیٹا بیس کے پاس ورڈ تبدیل کرنے کی ضرورت ہو تو، نیچے دیے گئے فارم کا استعمال کریں.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB ڈیٹا بیس کے انتظام آلے پر جانے';
+$lang['mariadb_management_tool_not_accessible'] = 'ڈیٹا بیس نہیں چل رہا ہے جب MariaDB ڈیٹا بیس کے انتظام آلے دستیاب نہیں ہے';
+$lang['mariadb_set_password'] = 'پاس ورڈ رکھیں';
