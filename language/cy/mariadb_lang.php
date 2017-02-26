20af7860c80962475809f8a6289b0e4b7cea04c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:30:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'Gweinydd Cronfa Ddata MariaDB';
+$lang['mariadb_app_description'] = 'MariaDB yn system cronfa ddata poblogaidd. Gellir ei ffurfweddu i redeg ceisiadau gyrru cronfa ddata, gwefannau, CRM ac yn ymarferol unrhyw adnodd arall sydd angen gwasanaeth storio perthynol.';
+$lang['mariadb_app_tooltip'] = 'Mae&#39;r cyfrif gwraidd a ddefnyddir gan y gronfa ddata yn wahanol na&#39;r un a ddefnyddir gan y system!';
+$lang['mariadb_mariadb_database'] = 'Cronfa Ddata MariaDB';
+$lang['mariadb_access_denied'] = 'Mae mynediad yn cael ei wrthod.';
+$lang['mariadb_password_updated'] = 'Diweddarwyd Cyfrinair yn llwyddiannus.';
+$lang['mariadb_username_invalid'] = 'Enw defnyddiwr yn annilys.';
+$lang['mariadb_hostname_invalid'] = 'Enw Gwesteiwr yn annilys.';
+$lang['mariadb_password_invalid'] = 'Cyfrinair yn annilys.';
+$lang['mariadb_password_mismatch'] = 'Cyfrinair a gwirio yn cyd-fynd.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Os gwelwch yn dda osod cyfrinair gronfa ddata.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storio';
+$lang['mariadb_management_tool'] = 'Offeryn Rheoli MariaDB';
+$lang['mariadb_management_tool_help'] = 'Dilynwch y ddolen i gael mynediad i&#39;r teclyn rheoli MariaDB. Os oes angen i newid eich cyfrinair cronfa ddata, os gwelwch yn dda defnyddiwch y ffurflen isod.';
+$lang['mariadb_go_to_management_tool'] = 'Ewch i Offeryn Rheoli Cronfa Ddata MariaDB';
+$lang['mariadb_management_tool_not_accessible'] = 'Nid yw&#39;r Pecyn Rheoli Cronfa Ddata MariaDB ar gael pan nad yw&#39;r gronfa ddata yn rhedeg';
+$lang['mariadb_set_password'] = 'set Password';
