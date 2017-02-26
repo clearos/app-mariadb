f6404734e588069e0cb6b489ecdcf9668f95f9b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:22:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB ne mai rare database tsarin. Ba za a iya kaga gudu database kore aikace-aikace, yanar, CRM da kusan duk wani hanya bukata a da zumunci ajiya sabis.';
+$lang['mariadb_app_tooltip'] = 'The tushen account amfani da database ne daban-daban fiye da daya amfani da tsarin!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Access aka hana.';
+$lang['mariadb_password_updated'] = 'Password updated samu nasarar.';
+$lang['mariadb_username_invalid'] = 'Sunan mai amfani ba daidai ba ne.';
+$lang['mariadb_hostname_invalid'] = 'Sunan mai masauki ba shi da inganci.';
+$lang['mariadb_password_invalid'] = 'Password ba daidai ba ne.';
+$lang['mariadb_password_mismatch'] = 'Password da kuma tabbatar da basu dace ba.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Don Allah saita database kalmar sirri.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Bi mahada don samun damar MariaDB management kayan aiki. Idan kana bukatar ka canza database kalmar sirri, don Allah yi amfani da tsari a kasa.';
+$lang['mariadb_go_to_management_tool'] = 'Ku To MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB Database Management Tool ba samuwa a lõkacin da database ba a guje';
+$lang['mariadb_set_password'] = 'Sa Password';
