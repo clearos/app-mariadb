c15204e81fb954219786bede2d19728fa2c88e8f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:24:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'ម៉ាស៊ីនបម្រើមូលដ្ឋានទិន្នន័យ MariaDB';
+$lang['mariadb_app_description'] = 'MariaDB គឺជាប្រព័ន្ធមូលដ្ឋានទិន្នន័យមួយដែលពេញនិយម។ វាអាចត្រូវបានកំណត់រចនាសម្ព័ន្ធដើម្បីរត់កម្មវិធីមូលដ្ឋានទិន្នន័យជំរុញ, វេបសាយ, អនុវត្ត CRM និងធនធានផ្សេងទៀតណាមួយដែលតម្រូវឱ្យមានសេវាផ្ទុកទំនាក់ទំនង។';
+$lang['mariadb_app_tooltip'] = 'គណនី root បានប្រើដោយមូលដ្ឋានទិន្នន័យនេះគឺខុសគ្នាជាងមួយដែលត្រូវបានប្រើដោយប្រព័ន្ធនេះ!';
+$lang['mariadb_mariadb_database'] = 'មូលដ្ឋានទិន្នន័យ MariaDB';
+$lang['mariadb_access_denied'] = 'ការចូលដំណើរការត្រូវបានបដិសេធ។';
+$lang['mariadb_password_updated'] = 'ពាក្យសម្ងាត់ដែលបានធ្វើបច្ចុប្បន្នភាពដោយជោគជ័យ។';
+$lang['mariadb_username_invalid'] = 'ឈ្មោះអ្នកប្រើមិនត្រឹមត្រូវ។';
+$lang['mariadb_hostname_invalid'] = 'ឈ្មោះម៉ាស៊ីនមិនត្រឹមត្រូវ។';
+$lang['mariadb_password_invalid'] = 'ពាក្យសម្ងាត់គឺមិនត្រឹមត្រូវ។';
+$lang['mariadb_password_mismatch'] = 'ពាក្យសម្ងាត់និងផ្ទៀងផ្ទាត់មិនត្រូវគ្នា។';
+$lang['mariadb_lang_please_set_a_database_password'] = 'សូមកំណត់ពាក្យសម្ងាត់មូលដ្ឋានទិន្នន័យមួយ។';
+$lang['mariadb_mariadb_storage'] = 'MariaDB ផ្ទុក';
+$lang['mariadb_management_tool'] = 'ឧបករណ៍គ្រប់គ្រង MariaDB';
+$lang['mariadb_management_tool_help'] = 'តំណភ្ជាប់ខាងក្រោមដើម្បីចូលដំណើរការឧបករណ៍គ្រប់គ្រង MariaDB នេះ។ ប្រសិនបើអ្នកត្រូវការផ្លាស់ប្តូរពាក្យសម្ងាត់មូលដ្ឋានទិន្នន័យរបស់អ្នកសូមប្រើសំណុំបែបបទខាងក្រោម។';
+$lang['mariadb_go_to_management_tool'] = 'ចូលទៅកាន់ឧបករណ៍គ្រប់គ្រងមូលដ្ឋានទិន្នន័យ MariaDB';
+$lang['mariadb_management_tool_not_accessible'] = 'ឧបករណ៍គ្រប់គ្រងមូលដ្ឋានទិន្នន័យ MariaDB គឺមិនមានទេនៅពេលដែលមូលដ្ឋានទិន្នន័យមិនកំពុងរត់';
+$lang['mariadb_set_password'] = 'កំណត់ពាក្យសម្ងាត់';
