e096ef997761290ef5f77546428752452611c6c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:25:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'Mariadb Database Server';
+$lang['mariadb_app_description'] = 'Mariadb ir populārs datu bāzes sistēmas. To var konfigurēt, lai palaistu datubāzi orientētu lietojumprogrammas, mājas lapas, CRM un praktiski jebkuru citu resursu prasa relāciju uzglabāšanas pakalpojumu.';
+$lang['mariadb_app_tooltip'] = 'Saknes konts datu bāzē tiek izmantota atšķirīga nekā tā, ko izmantotās sistēmas!';
+$lang['mariadb_mariadb_database'] = 'mariadb Database';
+$lang['mariadb_access_denied'] = 'Pieeja liegta.';
+$lang['mariadb_password_updated'] = 'Parole veiksmīgi atjaunināts.';
+$lang['mariadb_username_invalid'] = 'Lietotājvārds ir nederīgs.';
+$lang['mariadb_hostname_invalid'] = 'Resursdatora ir nederīgs.';
+$lang['mariadb_password_invalid'] = 'Parole ir nederīgs.';
+$lang['mariadb_password_mismatch'] = 'Parole un pārbaudīt nesakrīt.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Lūdzu noteikt datu bāzes paroli.';
+$lang['mariadb_mariadb_storage'] = 'mariadb Storage';
+$lang['mariadb_management_tool'] = 'Mariadb Management Tool';
+$lang['mariadb_management_tool_help'] = 'Sekojiet uz saites, lai piekļūtu mariadb pārvaldības rīks. Ja jums ir nepieciešams, lai mainītu savu datu bāzes paroli, lūdzu, izmantojiet zemāk esošo formu.';
+$lang['mariadb_go_to_management_tool'] = 'Iet uz mariadb datu bāzes vadības rīks';
+$lang['mariadb_management_tool_not_accessible'] = 'Mariadb Database Management Tool nav pieejama, ja datu bāze nedarbojas';
+$lang['mariadb_set_password'] = 'Set Password';
