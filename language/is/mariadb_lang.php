083737323f755923d608be9e661644641cec4417
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:23:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB er vinsæll gagnasafn kerfi. Það er hægt að stilla til að keyra gagnagrunn ekið forrit, vefsíður, CRM og nánast önnur úrræði sem krefjast Vensla geymsla þjónustu.';
+$lang['mariadb_app_tooltip'] = 'Rót reikning notuð af gagnagrunninum er öðruvísi en sú sem notuð af kerfinu!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Aðgangi er hafnað.';
+$lang['mariadb_password_updated'] = 'Lykilorð uppfærð.';
+$lang['mariadb_username_invalid'] = 'Notandanafn er ógilt.';
+$lang['mariadb_hostname_invalid'] = 'Hostname er ógilt.';
+$lang['mariadb_password_invalid'] = 'Lykilorð er ógilt.';
+$lang['mariadb_password_mismatch'] = 'Lykilorð og staðfesta passa ekki.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Vinsamlega settu gagnagrunn lykilorð.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Geymsla';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Fylgdu tenglinum til að fá aðgang að MariaDB stjórnun tól. Ef þú þarft að breyta gagnasafn lykilorðinu þínu, vinsamlegast notaðu eyðublaðið hér fyrir neðan.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB Database Management Tool er ekki í boði þegar gagnagrunnurinn er ekki í gangi';
+$lang['mariadb_set_password'] = 'Set Password';
