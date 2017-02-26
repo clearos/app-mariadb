678e8b2797b25916dd916de825bf704d82b0fbfc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:25:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB мэдээллийн сан Server';
+$lang['mariadb_app_description'] = 'MariaDB алдартай мэдээллийн санг систем юм. Энэ нь мэдээллийн баазад суурилсан програм, вэб хуудас, CRM болон бараг бусад нөөцийг нь хамааралт хадгалах үйлчилгээг шаардлагатай ажиллуулахын тулд тохируулж болно.';
+$lang['mariadb_app_tooltip'] = 'мэдээллийн санд ашиглаж эх дансны системд хэрэглэгддэг өөр юм!';
+$lang['mariadb_mariadb_database'] = 'MariaDB мэдээллийн сан';
+$lang['mariadb_access_denied'] = 'Хандалтын татгалзсан байна.';
+$lang['mariadb_password_updated'] = 'Нууц үг амжилттай шинэчиллээ.';
+$lang['mariadb_username_invalid'] = 'Хэрэглэгчийн нэр хүчин төгөлдөр бус байна.';
+$lang['mariadb_hostname_invalid'] = 'Hostname хүчин төгөлдөр бус байна.';
+$lang['mariadb_password_invalid'] = 'Нууц үг хүчин төгөлдөр бус байна.';
+$lang['mariadb_password_mismatch'] = 'Нууц үг болон таарахгүй байхгүй бол шалгана.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Мэдээллийн баазын нууц үгийг тохируулах уу.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB хадгалах';
+$lang['mariadb_management_tool'] = 'MariaDB менежментийн арга хэрэгсэл';
+$lang['mariadb_management_tool_help'] = 'MariaDB удирдлагын хэрэгсэл хандах холбоосыг дагана уу. Хэрэв та таны өгөгдлийн сангийн нууц үгээ солих хэрэгтэй бол доорх маягтыг ашиглана уу.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB Өгөгдлийн сангийн удирдлагын арга хэрэгсэл Go';
+$lang['mariadb_management_tool_not_accessible'] = 'мэдээллийн бааз ажиллаж байх үед MariaDB Өгөгдлийн сангийн удирдлагын арга хэрэгсэл байхгүй бол';
+$lang['mariadb_set_password'] = 'Set Нууц үг';
