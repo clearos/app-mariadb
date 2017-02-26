9d81c4ed15b51db687a9775fd2da5b90abcc8754
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:27:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'МариаДБ Датабасе Сервер';
+$lang['mariadb_app_description'] = 'МариаДБ је популаран систем база података. Може бити конфигурисан за покретање базама података апликација, веб, ЦРМ и практично било који други извор захтевају односну сервис за складиштење.';
+$lang['mariadb_app_tooltip'] = 'Рачун корен користи базе података је другачија од оне коју користи систем!';
+$lang['mariadb_mariadb_database'] = 'МариаДБ база података';
+$lang['mariadb_access_denied'] = 'Приступ је одбијен.';
+$lang['mariadb_password_updated'] = 'Пассворд упдатед суццессфулли.';
+$lang['mariadb_username_invalid'] = 'Корисничко име је неважећи.';
+$lang['mariadb_hostname_invalid'] = 'Хостнаме је неважећи.';
+$lang['mariadb_password_invalid'] = 'Лозинка је неважећи.';
+$lang['mariadb_password_mismatch'] = 'Лозинком и верификовати се не подударају.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Молимо поставите лозинку базе података.';
+$lang['mariadb_mariadb_storage'] = 'МариаДБ за складиштење';
+$lang['mariadb_management_tool'] = 'МариаДБ Манагемент Тоол';
+$lang['mariadb_management_tool_help'] = 'Пратите линк за приступ алат за управљање у МариаДБ. Ако је потребно да промените лозинку базе података, користите форму испод.';
+$lang['mariadb_go_to_management_tool'] = 'Иди на МариаДБ Тоол Датабасе Манагемент';
+$lang['mariadb_management_tool_not_accessible'] = 'Алат за управљање база МариаДБ није доступна када се база података није покренут';
+$lang['mariadb_set_password'] = 'Постави лозинку';
