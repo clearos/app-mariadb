234e0b9d5469bd389b1b6505785bb5365da55130
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:22:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB baz done sèvè';
+$lang['mariadb_app_description'] = 'MariaDB se yon sistèm baz done popilè. Li kapab configuré nan kouri baz done kondwi aplikasyon pou, sit entènèt, CRM ak pratikman nenpòt lòt resous ki mande yon sèvis depo relasyon.';
+$lang['mariadb_app_tooltip'] = 'Kont la rasin itilize pa baz done a se diferan pase yon sèl la itilize pa sistèm la!';
+$lang['mariadb_mariadb_database'] = 'MariaDB baz done';
+$lang['mariadb_access_denied'] = 'Aksè yo refize.';
+$lang['mariadb_password_updated'] = 'Modpas mete ajou avèk siksè.';
+$lang['mariadb_username_invalid'] = 'D &#39;se valab.';
+$lang['mariadb_hostname_invalid'] = 'Hostname se valab.';
+$lang['mariadb_password_invalid'] = 'Modpas se valab.';
+$lang['mariadb_password_mismatch'] = 'Modpas epi verifye pa matche ak.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Tanpri mete yon modpas baz done.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Depo';
+$lang['mariadb_management_tool'] = 'MariaDB Jesyon Zouti';
+$lang['mariadb_management_tool_help'] = 'Swiv lyen ki jwenn aksè nan zouti nan jesyon MariaDB. Si ou bezwen chanje modpas baz done ou, tanpri itilize fòm ki anba a.';
+$lang['mariadb_go_to_management_tool'] = 'Ale nan MariaDB Zouti Jesyon baz done';
+$lang['mariadb_management_tool_not_accessible'] = 'Jesyon baz done Zouti nan MariaDB a pa disponib lè baz done a pa ap kouri';
+$lang['mariadb_set_password'] = 'mete Modpas';
