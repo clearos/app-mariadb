7603f595eca4f1447d1d9056e5481abc34f6d008
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:25:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB हे डाटाबेस सर्व्हर';
+$lang['mariadb_app_description'] = 'MariaDB हे एक लोकप्रिय डेटाबेस प्रणाली आहे. तो एक संबंध संचय सेवा आवश्यक डेटाबेस चेंडू अनुप्रयोग, वेबसाइट, सी आर एम आणि प्रत्यक्ष व्यवहारात कोणत्याही इतर स्रोत चालवण्याची कॉन्फिगर केले जाऊ शकते.';
+$lang['mariadb_app_tooltip'] = 'डेटाबेस द्वारे वापरले रूट खाते प्रणाली द्वारे वापरले एक पेक्षा भिन्न आहे!';
+$lang['mariadb_mariadb_database'] = 'MariaDB हे डेटाबेस';
+$lang['mariadb_access_denied'] = 'प्रवेश नाकारला आहे.';
+$lang['mariadb_password_updated'] = 'पासवर्ड यशस्वीरित्या अद्यतनित केले.';
+$lang['mariadb_username_invalid'] = 'वापरकर्तानाव अवैध आहे.';
+$lang['mariadb_hostname_invalid'] = 'होस्ट नाव अवैध आहे.';
+$lang['mariadb_password_invalid'] = 'पासवर्ड अवैध आहे.';
+$lang['mariadb_password_mismatch'] = 'पासवर्ड आणि सत्यापित करा जुळत नाही.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'डेटाबेस पासवर्ड सेट करा.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB हे स्टोरेज';
+$lang['mariadb_management_tool'] = 'MariaDB हे व्यवस्थापन साधन';
+$lang['mariadb_management_tool_help'] = 'MariaDB हे व्यवस्थापन साधन प्रवेश करण्यासाठी दुवा अनुसरण. आपण आपल्या डेटाबेस संकेतशब्द बदलण्याची आवश्यकता आहे असल्यास, कृपया खालील फॉर्म वापरा.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB हे डेटाबेस व्यवस्थापन साधन जा';
+$lang['mariadb_management_tool_not_accessible'] = 'डेटाबेस चालत नाही तेव्हा MariaDB हे डेटाबेस व्यवस्थापन साधन उपलब्ध नाही';
+$lang['mariadb_set_password'] = 'सेट केलेला पासवर्ड';
