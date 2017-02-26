715cf07b22d243f82e4755866b7b224d16a4030f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:28:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB nyaéta sistem database populér. Hal ieu bisa ngonpigurasi pikeun ngajalankeun aplikasi database disetir, situs web, CRM jeung praktis nu mana wae nu sumberdaya séjén nu merlukeun layanan gudang relational.';
+$lang['mariadb_app_tooltip'] = 'akun akar dipaké ku database nu beda ti hiji nu dipaké ku sistem!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Access ieu ditolak.';
+$lang['mariadb_password_updated'] = 'Sandi junun diropéa.';
+$lang['mariadb_username_invalid'] = 'Username anu sah.';
+$lang['mariadb_hostname_invalid'] = 'Hostname téh sah.';
+$lang['mariadb_password_invalid'] = 'Sandi mangrupa salah.';
+$lang['mariadb_password_mismatch'] = 'Password jeung pariksa teu cocog.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Mangga nangtukeun kecap akses database.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'Alat Management MariaDB';
+$lang['mariadb_management_tool_help'] = 'Turutan link pikeun ngakses alat manajemén MariaDB. Lamun Anjeun kudu ngarobah password database, mangga make formulir di handap.';
+$lang['mariadb_go_to_management_tool'] = 'Pindah Pikeun Alat Management Database MariaDB';
+$lang['mariadb_management_tool_not_accessible'] = 'Nu Alat Management Database MariaDB teu sadia sawaktos database teu ngajalankeun';
+$lang['mariadb_set_password'] = 'susunan Password';
