01b49e8d7eeab13768a9336a78de3e56d9efbc4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:19:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Նյութերի բազա Server';
+$lang['mariadb_app_description'] = 'MariaDB է հայտնի տվյալների բազայի համակարգը: Այն կարող է կազմաձեւվել է առաջադրվել բազայի վրա հիմնված դիմումների, կայքերը, CRM եւ գործնականում ցանկացած այլ ռեսուրս պահանջող է Հարաբերական պահեստային ծառայություն:';
+$lang['mariadb_app_tooltip'] = 'The արմատ հաշիվն օգտագործվում է տվյալների բազայի տարբեր է, քան այն մեկը, որն օգտագործվում են համակարգի կողմից:';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Access մերժվում է.';
+$lang['mariadb_password_updated'] = 'Գաղտնաբառ թարմացվել հաջողությամբ.';
+$lang['mariadb_username_invalid'] = 'Մականուն անվավեր է:';
+$lang['mariadb_hostname_invalid'] = 'Հոսթի անվանում է անվավեր:';
+$lang['mariadb_password_invalid'] = 'Գաղտնաբառ անվավեր է:';
+$lang['mariadb_password_mismatch'] = 'Գաղտնաբառ եւ ստուգել չեն համընկնում:';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Խնդրում ենք սահմանել բազայի գաղտնաբառը:';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB կառավարման գործիք';
+$lang['mariadb_management_tool_help'] = 'Հետեւեք հղումը մուտք գործելու MariaDB կառավարման գործիք. Եթե ​​Ձեզ անհրաժեշտ է փոխել Ձեր բազայի գաղտնաբառը, խնդրում ենք օգտագործել ստորեւ բերված ձեւը.';
+$lang['mariadb_go_to_management_tool'] = 'Գնալ MariaDB տվյալների շտեմարանի կառավարման գործիք';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB տվյալների շտեմարանի կառավարման գործիք հասանելի չէ, երբ բազան չի վազում';
+$lang['mariadb_set_password'] = 'Set Գաղտնաբառերի';
