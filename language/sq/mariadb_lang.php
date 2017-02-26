03c36f1a2af8766f12dc55963f823885c4a17fad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:18:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Baza e të dhënave Server';
+$lang['mariadb_app_description'] = 'MariaDB është një sistem popullor bazës së të dhënave. Ajo mund të jetë konfiguruar për të drejtuar bazës së të dhënave të shtyrë aplikacione, faqet e internetit, CRM dhe praktikisht çdo burim tjetër që kërkojnë një shërbim të ruajtjes relacionale.';
+$lang['mariadb_app_tooltip'] = 'Llogaria Rrënja përdoret me bazën e të dhënave është e ndryshme se sa ajo e përdorur nga sistemi!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Baza e të dhënave';
+$lang['mariadb_access_denied'] = 'Hyrja e ndaluar.';
+$lang['mariadb_password_updated'] = 'Password rifreskua me sukses.';
+$lang['mariadb_username_invalid'] = 'Emri i përdoruesit është i pavlefshëm.';
+$lang['mariadb_hostname_invalid'] = 'Hostname është e pavlefshme.';
+$lang['mariadb_password_invalid'] = 'Fjalëkalimi është e pavlefshme.';
+$lang['mariadb_password_mismatch'] = 'Fjalëkalimi dhe të verifikojë nuk përputhen.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Ju lutemi të vendosur një fjalëkalim bazë të dhënash.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Menaxhimi Tool';
+$lang['mariadb_management_tool_help'] = 'Ndiqni lidhjen për të hyrë në mjet menaxhimi MariaDB. Nëse keni nevojë për të ndryshuar fjalëkalimin tuaj bazës së të dhënave, ju lutem, përdorni formularin më poshtë.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB Tool Baza e të dhënave të Menaxhimit';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB Database Management Tool nuk është në dispozicion, kur baza e të dhënave nuk është i rrjedhshëm';
+$lang['mariadb_set_password'] = 'Set Password';
