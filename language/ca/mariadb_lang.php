cab48a19bebf210628d5f56c7d939e159f101bd7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:20:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB base de dades del servidor';
+$lang['mariadb_app_description'] = 'MariaDB és un sistema de base de dades populars. Pot ser configurat per a engegar aplicacions amb bases de dades, llocs web, CRM i pràcticament qualsevol altre recurs que requereixen un servei d&#39;emmagatzematge relacional.';
+$lang['mariadb_app_tooltip'] = 'El compte root utilitzat per la base de dades és diferent de la utilitzada pel sistema!';
+$lang['mariadb_mariadb_database'] = 'Base de dades MariaDB';
+$lang['mariadb_access_denied'] = 'Accés denegat.';
+$lang['mariadb_password_updated'] = 'Contrasenya actualitzada correctament.';
+$lang['mariadb_username_invalid'] = 'Nom d&#39;usuari no és vàlid.';
+$lang['mariadb_hostname_invalid'] = 'Nom d&#39;amfitrió no és vàlid.';
+$lang['mariadb_password_invalid'] = 'Contrasenya no és vàlida.';
+$lang['mariadb_password_mismatch'] = 'Contrasenya i verificar no coincideixen.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Si us plau, establir una contrasenya de base de dades.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB emmagatzematge';
+$lang['mariadb_management_tool'] = 'Eina de gestió de MariaDB';
+$lang['mariadb_management_tool_help'] = 'Seguiu l&#39;enllaç per accedir a l&#39;eina de gestió MariaDB. Si necessita canviar la contrasenya de base de dades, si us plau utilitzeu el formulari a continuació.';
+$lang['mariadb_go_to_management_tool'] = 'Anar a MariaDB eina de gestió de base de dades';
+$lang['mariadb_management_tool_not_accessible'] = 'L&#39;eina de gestió de base de dades MariaDB no està disponible quan la base de dades no s&#39;està executant';
+$lang['mariadb_set_password'] = 'establir contrasenya';
