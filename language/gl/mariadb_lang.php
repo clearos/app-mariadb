52f74a80cf54a5797639e9521d738c09a3f36605
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:21:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB é un sistema de base de datos populares. Pode ser configurado para executar aplicacións base de datos orientado, sitios, CRM e practicamente calquera outro recurso que esixen un servizo de almacenamento relacional.';
+$lang['mariadb_app_tooltip'] = 'A conta de raíz utilizada pola base de datos é diferente do que a utilizada polo sistema!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Base de Datos';
+$lang['mariadb_access_denied'] = 'Acceso denegado.';
+$lang['mariadb_password_updated'] = 'Contrasinal actualizada correctamente.';
+$lang['mariadb_username_invalid'] = 'Nome de usuario non é válido.';
+$lang['mariadb_hostname_invalid'] = 'Hostname é válido.';
+$lang['mariadb_password_invalid'] = 'Contrasinal é incorrecto.';
+$lang['mariadb_password_mismatch'] = 'Contrasinal e comprobar non coinciden.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Por favor, configure un contrasinal de base de datos.';
+$lang['mariadb_mariadb_storage'] = 'almacenamento MariaDB';
+$lang['mariadb_management_tool'] = 'Ferramenta de Xestión MariaDB';
+$lang['mariadb_management_tool_help'] = 'Siga a ligazón para acceder a ferramenta de xestión de MariaDB. Se precisa mudar o seu contrasinal de base de datos, use o formulario de embaixo.';
+$lang['mariadb_go_to_management_tool'] = 'Ir ao MariaDB Ferramenta de Xestión de Base de Datos';
+$lang['mariadb_management_tool_not_accessible'] = 'A Ferramenta de Xestión de Base de Datos MariaDB non está dispoñible cando a base de datos non está en execución';
+$lang['mariadb_set_password'] = 'Fixar contrasinal';
