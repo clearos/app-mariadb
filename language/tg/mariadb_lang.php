065528c9eff1c4e4cb2279219a326ce0c2b2231f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:29:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Махзани Server';
+$lang['mariadb_app_description'] = 'MariaDB системаи махзани машҳур аст. Он метавонад танзим ба кор базаи ронда барномаҳо, сомонаҳо, CRM ва амалан ягон манбаи дигаре, ки ниёзманди хадамоти нигаҳдории ин муносибати.';
+$lang['mariadb_app_tooltip'] = 'Ба ҳисоби реша истифода махзани аз касе истифода дар системаи гуногун аст!';
+$lang['mariadb_mariadb_database'] = 'Махзани маълумоти MariaDB';
+$lang['mariadb_access_denied'] = 'Дастрасӣ манъ аст.';
+$lang['mariadb_password_updated'] = 'Парол нав карда мешавад.';
+$lang['mariadb_username_invalid'] = 'Логин нодуруст аст.';
+$lang['mariadb_hostname_invalid'] = 'Мизбони нодуруст аст.';
+$lang['mariadb_password_invalid'] = 'Парол нодуруст аст.';
+$lang['mariadb_password_mismatch'] = 'Рамз ва тасдиқ мекунем мувофиқат намекунанд.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Марҳамат карда гузарвожаи базаи муқаррар карда мешавад.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB гардероб';
+$lang['mariadb_management_tool'] = 'Tool идораи MariaDB';
+$lang['mariadb_management_tool_help'] = 'Пайравӣ пайванд дастрасӣ ба воситаи идоракунии MariaDB. Агар ба шумо лозим тағйир парол базаи худро, лутфан зер истифода шакли.';
+$lang['mariadb_go_to_management_tool'] = 'Гузаштан ба MariaDB Tool идораи Махзани';
+$lang['mariadb_management_tool_not_accessible'] = 'Дар Tool идораи Махзани маълумоти MariaDB дастрас нест, вақте ки базаи иҷро нашуда';
+$lang['mariadb_set_password'] = 'Танзими парол';
