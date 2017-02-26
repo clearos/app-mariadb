e808a75c18cb40c447c05d7b19d777ac72b0e0cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:29:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'Mariadb databasserver';
+$lang['mariadb_app_description'] = 'Mariadb är en populär databassystem. Den kan konfigureras för att köra databasdrivna applikationer, webbplatser, CRM och praktiskt taget alla andra resurser kräver en relationslagringstjänst.';
+$lang['mariadb_app_tooltip'] = 'Root-kontot som används av databasen är annorlunda än den som används av systemet!';
+$lang['mariadb_mariadb_database'] = 'mariadb Databas';
+$lang['mariadb_access_denied'] = 'Åtkomst nekad.';
+$lang['mariadb_password_updated'] = 'Lösenord uppdaterats.';
+$lang['mariadb_username_invalid'] = 'Användarnamnet är ogiltigt.';
+$lang['mariadb_hostname_invalid'] = 'Värdnamn är ogiltigt.';
+$lang['mariadb_password_invalid'] = 'Lösenordet är ogiltigt.';
+$lang['mariadb_password_mismatch'] = 'Lösenord och verifiera stämmer inte överens.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Ställ in en databas lösenord.';
+$lang['mariadb_mariadb_storage'] = 'mariadb Lagring';
+$lang['mariadb_management_tool'] = 'Mariadb Management Tool';
+$lang['mariadb_management_tool_help'] = 'Följ länken för att komma åt mariadb verktyg. Om du behöver ändra din databas lösenord, vänligen använd formuläret nedan.';
+$lang['mariadb_go_to_management_tool'] = 'Gå till mariadb Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'Den mariadb Database Management Tool är inte tillgänglig när databasen inte är igång';
+$lang['mariadb_set_password'] = 'Välj lösenord';
