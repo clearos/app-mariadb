54855757eb7ecfb8c56806e35d8320af5f55330a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:23:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB bụ a na-ewu ewu nchekwa data usoro. Ọ nwere ike ahazi na-agba ọsọ nchekwa data chụpụrụ ngwa, weebụsaịtị, CRM na ihe fọrọ nke ọ bụla ọzọ akụ nke chọrọ ka a relational nchekwa ọrụ.';
+$lang['mariadb_app_tooltip'] = 'The mgbọrọgwụ akaụntụ ji site nchekwa data bụ dị iche iche karịa onye na-eji usoro!';
+$lang['mariadb_mariadb_database'] = 'MariaDB database';
+$lang['mariadb_access_denied'] = 'Access na-agọ.';
+$lang['mariadb_password_updated'] = 'Password emelitere ọma.';
+$lang['mariadb_username_invalid'] = 'Aha njirimara bụ ghara ịdị irè.';
+$lang['mariadb_hostname_invalid'] = 'Hostname bụ ghara ịdị irè.';
+$lang['mariadb_password_invalid'] = 'Password bụ ghara ịdị irè.';
+$lang['mariadb_password_mismatch'] = 'Password na nyochaa njirimara amakọghị.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Biko ịtọ a nchekwa data paswọọdụ.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Nchekwa';
+$lang['mariadb_management_tool'] = 'MariaDB Management Ngwá Ọrụ';
+$lang['mariadb_management_tool_help'] = 'Soro njikọ ka ịnweta MariaDB management ngwá ọrụ. Ọ bụrụ na ị chọrọ ịgbanwe gị nchekwa data paswọọdụ, biko jiri ụdị n&#39;okpuru.';
+$lang['mariadb_go_to_management_tool'] = 'Gaa MariaDB Database Management Ngwá Ọrụ';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB Database Management Ngwá Ọrụ a adịghị mgbe nchekwa data na-adịghị na-agba ọsọ';
+$lang['mariadb_set_password'] = 'set Password';
