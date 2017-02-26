c048f1aad691e2ca909ad574f646a323db27d6b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:27:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB é um sistema de banco de dados popular. Ele pode ser configurado para executar aplicativos banco de dados orientado, sites, CRM e praticamente qualquer outro recurso que exigem um serviço de armazenamento relacional.';
+$lang['mariadb_app_tooltip'] = 'A conta de raiz utilizada pela base de dados é diferente do que a utilizada pelo sistema!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Banco de Dados';
+$lang['mariadb_access_denied'] = 'Acesso negado.';
+$lang['mariadb_password_updated'] = 'Senha atualizada com sucesso.';
+$lang['mariadb_username_invalid'] = 'Nome de usuário Inválido.';
+$lang['mariadb_hostname_invalid'] = 'Hostname é inválido.';
+$lang['mariadb_password_invalid'] = 'Senha é inválida.';
+$lang['mariadb_password_mismatch'] = 'Senha e verificar não coincidem.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Por favor, defina uma senha de banco de dados.';
+$lang['mariadb_mariadb_storage'] = 'Armazenamento MariaDB';
+$lang['mariadb_management_tool'] = 'Ferramenta de Gestão MariaDB';
+$lang['mariadb_management_tool_help'] = 'Siga o link para acessar a ferramenta de gerenciamento de MariaDB. Se você precisar alterar a sua senha de banco de dados, utilize o formulário abaixo.';
+$lang['mariadb_go_to_management_tool'] = 'Ir para o MariaDB Ferramenta de Gerenciamento de Banco de Dados';
+$lang['mariadb_management_tool_not_accessible'] = 'A Ferramenta de Gerenciamento de Banco de Dados MariaDB não está disponível quando o banco de dados não está sendo executado';
+$lang['mariadb_set_password'] = 'Configurar senha';
