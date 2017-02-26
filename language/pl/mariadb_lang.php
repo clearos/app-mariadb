1f769db38d4cc8c587218534ec1552d327d5084b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:27:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Server Database';
+$lang['mariadb_app_description'] = 'MariaDB to popularny system bazy danych. Może być skonfigurowany do uruchamiania aplikacji bazodanowych, strony internetowe, CRM i praktycznie każdy inny zasób wymagających relacyjnej przechowalnię.';
+$lang['mariadb_app_tooltip'] = 'Konto root wykorzystywane przez bazę danych różni się od tej stosowanej przez system!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Odmowa dostępu.';
+$lang['mariadb_password_updated'] = 'Hasło zostało zaktualizowane pomyślnie.';
+$lang['mariadb_username_invalid'] = 'Nazwa użytkownika jest niepoprawna.';
+$lang['mariadb_hostname_invalid'] = 'Nazwa hosta jest nieprawidłowa.';
+$lang['mariadb_password_invalid'] = 'Hasło jest błędne.';
+$lang['mariadb_password_mismatch'] = 'Hasło i weryfikacji nie pasują.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Proszę ustawić hasło bazy danych.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB bagażu';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Śledź ten link, aby uzyskać dostęp do funkcji zarządzania MariaDB. Jeśli trzeba zmienić hasło do bazy danych, skorzystaj z poniższego formularza.';
+$lang['mariadb_go_to_management_tool'] = 'Przejdź do MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB Database Management Tool nie jest dostępna, gdy baza danych nie jest uruchomiony';
+$lang['mariadb_set_password'] = 'Ustaw hasło';
