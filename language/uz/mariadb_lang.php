bb127170115dfaa4542c422e99fd9c883a75d95b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:30:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB bazasi Server';
+$lang['mariadb_app_description'] = 'MariaDB bir mashhur bazasi tizimi. Bu ilişkisel saqlash xizmati talab bazasi gijgijlash dasturlar, veb-saytlar, CRM va amaliy har qanday boshqa resurs ishlatish uchun tuzilgan bo&#39;lishi mumkin.';
+$lang['mariadb_app_tooltip'] = 'ma&#39;lumotlar bazasi tomonidan ishlatiladigan root hisobi tizimi tomonidan foydalaniladi kimsadan farq qiladi!';
+$lang['mariadb_mariadb_database'] = 'MariaDB bazasi';
+$lang['mariadb_access_denied'] = 'Kirish taqiqlandi.';
+$lang['mariadb_password_updated'] = 'Parol muvaffaqiyatli yangilandi.';
+$lang['mariadb_username_invalid'] = 'Foydalanuvchi nomi haqiqiy emas.';
+$lang['mariadb_hostname_invalid'] = 'Hostname haqiqiy emas.';
+$lang['mariadb_password_invalid'] = 'Parol yaroqsiz hisoblanadi.';
+$lang['mariadb_password_mismatch'] = 'Parol va mos emas tekshirish.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Ma&#39;lumotlar bazasi parol o&#39;rnatsangiz iltimos.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB saqlash';
+$lang['mariadb_management_tool'] = 'MariaDB boshqarish Vositachi';
+$lang['mariadb_management_tool_help'] = 'MariaDB boshqarish vositasi kirish uchun bog&#39;dan rioya qiling. Agar ma&#39;lumotlar bazasi parolni o&#39;zgartirish kerak bo&#39;lsa, quyidagi shakl foydalaning.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB ma&#39;lumotlar bazasi boshqarish vositasi uchun borib';
+$lang['mariadb_management_tool_not_accessible'] = 'ma&#39;lumotlar bazasi va hkz paytda MariaDB bazasi boshqarish Vositachi mavjud emas';
+$lang['mariadb_set_password'] = 'Parolni o&#39;rnatish';
