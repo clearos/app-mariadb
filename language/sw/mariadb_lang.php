ca1754c680ded129d1efb3fa1db6e5b24ed1f4c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:28:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB ni maarufu database mfumo. Ni inaweza kimeundwa kukimbia database inaendeshwa maombi, tovuti, ali na rasilimali nyingine yoyote kivitendo wanaohitaji huduma kimahusiano hifadhi.';
+$lang['mariadb_app_tooltip'] = 'akaunti mzizi kutumiwa na database ni tofauti na iliyotumiwa na mfumo!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Upatikanaji ni alikanusha.';
+$lang['mariadb_password_updated'] = 'Nywila updated mafanikio.';
+$lang['mariadb_username_invalid'] = 'Jina la mtumiaji ni batili.';
+$lang['mariadb_hostname_invalid'] = 'Jina la mpangishaji ni batili.';
+$lang['mariadb_password_invalid'] = 'Nywila ni batili.';
+$lang['mariadb_password_mismatch'] = 'Password na kuthibitisha si mechi.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Tafadhali kuweka database password.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Uhifadhi';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Kufuata kiungo kwa upatikanaji MariaDB usimamizi wa chombo. Kama unahitaji kubadilisha database password yako, tafadhali kutumia fomu ya chini.';
+$lang['mariadb_go_to_management_tool'] = 'Go MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB Database Management Tool haipatikani wakati database si mbio';
+$lang['mariadb_set_password'] = 'kuweka Nywila';
