985dae0c84527b5579adcf1a6c9ec59028801e9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:22:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB adalah sistem database yang populer. Hal ini dapat dikonfigurasi untuk menjalankan aplikasi database driven, website, CRM dan hampir semua sumber daya lain yang membutuhkan layanan penyimpanan relasional.';
+$lang['mariadb_app_tooltip'] = 'account root yang digunakan oleh database berbeda dari yang digunakan oleh sistem!';
+$lang['mariadb_mariadb_database'] = 'MariaDB database';
+$lang['mariadb_access_denied'] = 'Akses ditolak.';
+$lang['mariadb_password_updated'] = 'Sandi berhasil diperbarui.';
+$lang['mariadb_username_invalid'] = 'Nama pengguna tidak sah.';
+$lang['mariadb_hostname_invalid'] = 'Hostname tidak valid.';
+$lang['mariadb_password_invalid'] = 'Sandi tidak valid.';
+$lang['mariadb_password_mismatch'] = 'Password dan memverifikasi tidak cocok.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Silakan menetapkan password database.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'Alat Manajemen MariaDB';
+$lang['mariadb_management_tool_help'] = 'Ikuti link untuk mengakses alat manajemen MariaDB. Jika Anda perlu mengubah password database Anda, gunakan formulir di bawah ini.';
+$lang['mariadb_go_to_management_tool'] = 'Pergi Untuk Alat Database Management MariaDB';
+$lang['mariadb_management_tool_not_accessible'] = 'Alat Manajemen Database MariaDB tidak tersedia ketika database tidak berjalan';
+$lang['mariadb_set_password'] = 'set password';
