be68eb06328bcf51333cd6ef1ca4fd5ae137a64c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:25:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB dia malaza rafitra banky angona. Azo mampihaino azy mihazakazaka banky angona entin&#39;ny fampiharana, tranonkala, CRM, ary saika loharano hafa mitaky ny fifandraisana fitahirizana fanompoana.';
+$lang['mariadb_app_tooltip'] = 'Ny fakany kaonty ampiasain&#39;ny ny banky angona dia hafa noho ny iray ampiasain&#39;ny rafitra!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Access no nandà.';
+$lang['mariadb_password_updated'] = 'Password nohavaozina soa aman-tsara.';
+$lang['mariadb_username_invalid'] = 'Solonanarana dia tsy manan-kery.';
+$lang['mariadb_hostname_invalid'] = 'Hostname dia tsy manan-kery.';
+$lang['mariadb_password_invalid'] = 'Password no tsy manan-kery.';
+$lang['mariadb_password_mismatch'] = 'Password ary manamarina tsy mitovy.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Mba hametraka ny base de données tenimiafina.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Management Fitaovana';
+$lang['mariadb_management_tool_help'] = 'Araho ny rohy mba mahazo ny MariaDB fitantanana ny fitaovana. Raha toa ka mila ny hanova ny banky angona tenimiafina, mampiasa ny teny azafady etsy ambany.';
+$lang['mariadb_go_to_management_tool'] = 'Mandehana To MariaDB Database Management Fitaovana';
+$lang['mariadb_management_tool_not_accessible'] = 'Ny MariaDB Database Fitaovana fitantanana dia tsy misy raha tsy ny banky angona mihazakazaka';
+$lang['mariadb_set_password'] = 'Set Password';
