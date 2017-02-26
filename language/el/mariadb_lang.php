bb583e25aff575083861dd6d6541d8d448a5260f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:22:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB είναι ένα δημοφιλές σύστημα βάσης δεδομένων. Μπορεί να ρυθμιστεί ώστε να τρέξει βάση δεδομένων με γνώμονα τις εφαρμογές, ιστοσελίδες, CRM και σχεδόν κάθε άλλο πόρο που απαιτεί μια σχεσιακή υπηρεσία αποθήκευσης.';
+$lang['mariadb_app_tooltip'] = 'Ο λογαριασμός αυτός χρησιμοποιείται από τη βάση δεδομένων είναι διαφορετικό από εκείνο που χρησιμοποιείται από το σύστημα!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Η πρόσβαση απαγορεύεται.';
+$lang['mariadb_password_updated'] = 'Κωδικός πρόσβασης ενημερώθηκε με επιτυχία.';
+$lang['mariadb_username_invalid'] = 'Το όνομα χρήστη δεν είναι έγκυρο.';
+$lang['mariadb_hostname_invalid'] = 'Όνομα κεντρικού υπολογιστή δεν είναι έγκυρο.';
+$lang['mariadb_password_invalid'] = 'Κωδικός πρόσβασης δεν είναι έγκυρος.';
+$lang['mariadb_password_mismatch'] = 'Κωδικό πρόσβασης και επιβεβαιώστε δεν ταιριάζουν.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Παρακαλούμε να ορίσετε έναν κωδικό πρόσβασης βάσης δεδομένων.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB αποθήκευσης';
+$lang['mariadb_management_tool'] = 'Εργαλείο Διαχείρισης MariaDB';
+$lang['mariadb_management_tool_help'] = 'Ακολουθήστε το σύνδεσμο για να αποκτήσετε πρόσβαση στο εργαλείο διαχείρισης MariaDB. Εάν πρέπει να αλλάξετε τον κωδικό πρόσβασης βάσης δεδομένων σας, παρακαλούμε χρησιμοποιήστε την παρακάτω φόρμα.';
+$lang['mariadb_go_to_management_tool'] = 'Πηγαίνετε να MariaDB Εργαλείο Διαχείρισης Βάσεων Δεδομένων';
+$lang['mariadb_management_tool_not_accessible'] = 'Το Εργαλείο Διαχείρισης Βάσεων Δεδομένων MariaDB δεν είναι διαθέσιμη όταν η βάση δεδομένων δεν λειτουργεί';
+$lang['mariadb_set_password'] = 'Ορίστε κωδικό';
