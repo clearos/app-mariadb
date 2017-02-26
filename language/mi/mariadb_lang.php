4d897f4c297c08f596f71e8204729ddb447c0f43
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:25:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'Tūmau Pātengi Raraunga MariaDB';
+$lang['mariadb_app_description'] = 'MariaDB Ko te pūnaha pātengi raraunga rongonui. Ka taea te whirihora te reira ki te whakahaere i ngā tono pātengi raraunga aia, tukutuku, CRM me ngā tetahi atu rauemi tono he ratonga rokiroki tūhonohono.';
+$lang['mariadb_app_tooltip'] = 'Ko rerekē atu te tangata e whakamahia ana e te pūnaha te pūkete pakiaka whakamahia e te pātengi raraunga!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Pātengi Raraunga';
+$lang['mariadb_access_denied'] = 'whakakahore Access te.';
+$lang['mariadb_password_updated'] = 'whakahoutia pai te Kupuhipa.';
+$lang['mariadb_username_invalid'] = 'He muhu kaiwhakamahi.';
+$lang['mariadb_hostname_invalid'] = 'He muhu pūtaurima.';
+$lang['mariadb_password_invalid'] = 'He muhu te kupuhipa.';
+$lang['mariadb_password_mismatch'] = 'Kupuhipa a manatoko kaua e ōrite.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Koa whakaturia he kupuhipa pātengi raraunga.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'Tool Whakahaere MariaDB';
+$lang['mariadb_management_tool_help'] = 'A pee i te hono ki te uru i te taputapu whakahaere MariaDB. Ki te hiahia koe ki te huri i tō kupuhipa pātengi raraunga, tēnā whakamahi i te puka i raro nei.';
+$lang['mariadb_go_to_management_tool'] = 'Haere ki te Tool Whakahaere Pātengi MariaDB';
+$lang['mariadb_management_tool_not_accessible'] = 'E kore te mea e wātea ana te Tool Whakahaere Database MariaDB ka kore te rere i te pātengi raraunga';
+$lang['mariadb_set_password'] = 'Tautuhi Kupuhipa';
