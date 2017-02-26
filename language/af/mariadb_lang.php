da95d681db6b0eac8250e5202f472f8f58881a3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:18:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB databasis bediener';
+$lang['mariadb_app_description'] = 'MariaDB is &#39;n gewilde databasis stelsel. Dit kan ingestel word om die databasis-gedrewe programme, websites, CRM en amper &#39;n ander bron wat &#39;n relasionele stoor diens hardloop.';
+$lang['mariadb_app_tooltip'] = 'Die wortel rekening wat gebruik word deur die databasis is anders as die een wat deur die stelsel!';
+$lang['mariadb_mariadb_database'] = 'MariaDB databasis';
+$lang['mariadb_access_denied'] = 'Toegang word geweier.';
+$lang['mariadb_password_updated'] = 'Wagwoord suksesvol opgedateer.';
+$lang['mariadb_username_invalid'] = 'Gebruikersnaam is ongeldig.';
+$lang['mariadb_hostname_invalid'] = 'Gasheernaam is ongeldig.';
+$lang['mariadb_password_invalid'] = 'Wagwoord is ongeldig.';
+$lang['mariadb_password_mismatch'] = 'Wagwoord en verifieer stem nie ooreen nie.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Stel &#39;n databasis wagwoord.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB berging';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Volg die skakel om toegang te verkry tot die MariaDB beheer instrument. As jy nodig het om jou databasis wagwoord te verander, gebruik asseblief die vorm hieronder.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'Die MariaDB Database Management Tool is nie beskikbaar toe die databasis nie aktief';
+$lang['mariadb_set_password'] = 'stel wagwoord';
