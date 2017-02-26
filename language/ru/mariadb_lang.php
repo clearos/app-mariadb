ff2e552a8bc1c6387b1473f100cc580a0baa6da2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:27:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB является популярной системой баз данных. Он может быть настроен для работы на основе баз данных приложений, веб-сайты, CRM и практически любой другой ресурс, требующих реляционную сервис хранения.';
+$lang['mariadb_app_tooltip'] = 'Корневая учетная запись в базе данных, отличается от той, которая используется в системе!';
+$lang['mariadb_mariadb_database'] = 'MariaDB базы данных';
+$lang['mariadb_access_denied'] = 'В доступе отказано.';
+$lang['mariadb_password_updated'] = 'Пароль успешно обновлен.';
+$lang['mariadb_username_invalid'] = 'Имя пользователя недействительно.';
+$lang['mariadb_hostname_invalid'] = 'Hostname является недействительным.';
+$lang['mariadb_password_invalid'] = 'Неправильный пароль.';
+$lang['mariadb_password_mismatch'] = 'Пароль и проверить, не совпадают.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Пожалуйста, установите пароль базы данных.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB хранения';
+$lang['mariadb_management_tool'] = 'Инструмент управления MariaDB';
+$lang['mariadb_management_tool_help'] = 'Перейдите по ссылке, чтобы получить доступ к инструменту управления MariaDB. Если вам необходимо изменить пароль базы данных, пожалуйста, используйте форму ниже.';
+$lang['mariadb_go_to_management_tool'] = 'Перейти к MariaDB инструмент управления базами данных';
+$lang['mariadb_management_tool_not_accessible'] = 'Управление базой данных Инструмент MariaDB недоступна, если база данных не запущена';
+$lang['mariadb_set_password'] = 'Установка пароля';
