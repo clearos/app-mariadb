e8c6b530cf1fcbbd5787eab618494e32bcb32d83
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:23:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Freastalaí Bunachar Sonraí';
+$lang['mariadb_app_description'] = 'Is MariaDB córas bunachar tóir. Is féidir é a chumrú a reáchtáil bunachar tiomáinte iarratais, láithreáin ghréasáin, CRM agus go praiticiúil aon acmhainn eile a mbeidh gá le seirbhís stórála choibhneasta.';
+$lang['mariadb_app_tooltip'] = 'Is é an cuntas fréimhe a úsáideann an bunachar sonraí éagsúla ná an ceann a úsáidtear ag an gcóras!';
+$lang['mariadb_mariadb_database'] = 'Bunachar MariaDB';
+$lang['mariadb_access_denied'] = 'Rochtain Diúltaítear.';
+$lang['mariadb_password_updated'] = 'Pasfhocal suas chun dáta go rathúil.';
+$lang['mariadb_username_invalid'] = 'Is ainm úsáideora neamhbhailí.';
+$lang['mariadb_hostname_invalid'] = 'Is óstainm neamhbhailí.';
+$lang['mariadb_password_invalid'] = 'Focal faire neamhbhailí.';
+$lang['mariadb_password_mismatch'] = 'Pasfhocal agus a fhíorú nach bhfuil comhoiriúnach.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Socraigh focal faire bunachar sonraí.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Stóráil';
+$lang['mariadb_management_tool'] = 'Uirlis Bhainisteoireachta MariaDB';
+$lang['mariadb_management_tool_help'] = 'Lean an nasc chun teacht ar an uirlis bainistíochta MariaDB. Más gá duit a athrú ar do phasfhocal bhunachar sonraí, bain úsáid as an bhfoirm thíos.';
+$lang['mariadb_go_to_management_tool'] = 'Téigh go MariaDB Uirlis Bhainisteoireachta Bunachar Sonraí';
+$lang['mariadb_management_tool_not_accessible'] = 'Níl an MariaDB Uirlis Bainistíochta Bunachar Sonraí ar fáil nuair nach bhfuil an bunachar sonraí ag rith';
+$lang['mariadb_set_password'] = 'Pasfhocal Socraigh';
