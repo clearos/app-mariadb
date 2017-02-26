3a130297d569155002278bbd38b9f7c9ec86aee6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:28:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB je priljubljen sistem baze podatkov. To je mogoče konfigurirati za zagon baze podatkov usmerjenih aplikacij, spletnih strani, CRM in praktično nobene druge vire, ki zahtevajo relacijske shrambo.';
+$lang['mariadb_app_tooltip'] = 'Račun koren ga baze uporabili je drugačen od tistega, ki ga uporabljenega sistema!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Dostop zavrnjen.';
+$lang['mariadb_password_updated'] = 'Geslo uspešno posodobljen.';
+$lang['mariadb_username_invalid'] = 'Ime je neveljavna.';
+$lang['mariadb_hostname_invalid'] = 'Ime je neveljavna.';
+$lang['mariadb_password_invalid'] = 'Geslo je neveljavna.';
+$lang['mariadb_password_mismatch'] = 'Geslo in preveri se ne ujemata.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Prosim, nastavite geslo baze podatkov.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB shranjevanje';
+$lang['mariadb_management_tool'] = 'Orodje za upravljanje MariaDB';
+$lang['mariadb_management_tool_help'] = 'Sledite povezavi za dostop do orodja za upravljanje MariaDB. Če želite spremeniti geslo baze podatkov, prosimo uporabite spodnji obrazec.';
+$lang['mariadb_go_to_management_tool'] = 'Na MariaDB za upravljanje podatkovnih baz orodje';
+$lang['mariadb_management_tool_not_accessible'] = 'Uprava Database Orodje MariaDB ni na voljo, ko je baza podatkov ne teče';
+$lang['mariadb_set_password'] = 'Set Password';
