5861e85a6766d0de2ac046baa4735c9d5990b370
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:19:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB لخادم قاعدة البيانات';
+$lang['mariadb_app_description'] = 'MariaDB لهو نظام قاعدة بيانات شعبية. ويمكن تهيئتها لتشغيل قاعدة بيانات مدفوعة التطبيقات والمواقع الإلكترونية وإدارة علاقات العملاء وعمليا أي مورد آخر تتطلب خدمة تخزين العلائقية.';
+$lang['mariadb_app_tooltip'] = 'حساب الجذر المستخدمة في قاعدة بيانات مختلفة عن تلك المستخدمة من قبل النظام!';
+$lang['mariadb_mariadb_database'] = 'قاعدة بيانات MariaDB ل';
+$lang['mariadb_access_denied'] = 'الدخول محظور.';
+$lang['mariadb_password_updated'] = 'تم تحديث كلمة السر بنجاح.';
+$lang['mariadb_username_invalid'] = 'إسم المستخدم غير صحيح.';
+$lang['mariadb_hostname_invalid'] = 'اسم المضيف غير صالح.';
+$lang['mariadb_password_invalid'] = 'كلمة المرور غير صالحة.';
+$lang['mariadb_password_mismatch'] = 'كلمة المرور و التحقق من لا تتطابق.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'يرجى تعيين كلمة مرور قاعدة البيانات.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB لتخزين';
+$lang['mariadb_management_tool'] = 'أداة إدارة MariaDB ل';
+$lang['mariadb_management_tool_help'] = 'اتبع الرابط للوصول إلى أداة لإدارة MariaDB ل. إذا كنت بحاجة إلى تغيير كلمة مرور قاعدة البيانات الخاصة بك، يرجى استخدام النموذج أدناه.';
+$lang['mariadb_go_to_management_tool'] = 'الذهاب إلى أداة إدارة قواعد البيانات MariaDB ل';
+$lang['mariadb_management_tool_not_accessible'] = 'أداة إدارة قواعد البيانات MariaDB ليست متوفرة عند عدم تشغيل قاعدة البيانات';
+$lang['mariadb_set_password'] = 'ضبط كلمة السر';
