f27e208e8311f26c9b89597a4d24237ceb6e9356
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:20:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Nawonso achichepere Server';
+$lang['mariadb_app_description'] = 'MariaDB ndi otchuka Nawonso achichepere dongosolo. Zikhoza kukhazikitsidwa kuthamanga Nawonso achichepere lotengeka ntchito, Websites, CRM ndi gwero lina lililonse pafupifupi zofuna utumiki ubale yosungirako.';
+$lang['mariadb_app_tooltip'] = 'Nkhani muzu ntchito Nawonso achichepere ndi wosiyana kuposa ntchito dongosolo!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Nawonso achichepere';
+$lang['mariadb_access_denied'] = 'Access ndi adakana.';
+$lang['mariadb_password_updated'] = 'Achinsinsi kusinthidwa bwinobwino.';
+$lang['mariadb_username_invalid'] = 'Lolowera ndi opanda pake.';
+$lang['mariadb_hostname_invalid'] = 'Hostname ndi opanda pake.';
+$lang['mariadb_password_invalid'] = 'Achinsinsi ndi opanda pake.';
+$lang['mariadb_password_mismatch'] = 'Achinsinsi ndi kutsimikizira sizikugwirizana.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Chonde ikani Nawonso achichepere achinsinsi.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB yosungirako';
+$lang['mariadb_management_tool'] = 'MariaDB Management Chida';
+$lang['mariadb_management_tool_help'] = 'Kutsatira kugwirizana kuti kulumikiza MariaDB kasamalidwe chida. Ngati mukufuna kusintha Nawonso achichepere achinsinsi anu, chonde ntchito mawonekedwe m&#39;munsimu.';
+$lang['mariadb_go_to_management_tool'] = 'Pitani Kuti MariaDB Nawonso achichepere Management Chida';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB Nawonso achichepere Management Chida palibe pamene Nawonso achichepere si kuthamanga';
+$lang['mariadb_set_password'] = 'Anatipatsa Achinsinsi';
