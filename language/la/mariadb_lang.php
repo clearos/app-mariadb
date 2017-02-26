512210ff2376508e698d26e21a722b35379db7bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:24:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'Servo MariaDB Database';
+$lang['mariadb_app_description'] = 'MariaDB database system est a popularis. Potest pulsus configured ut run applications database, websites, et CRM fere alia ulla copia relationis repono servitium exigat.';
+$lang['mariadb_app_tooltip'] = 'Radix ratio per quam ab alia ratio datorum!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Obvius negavit est.';
+$lang['mariadb_password_updated'] = 'Password updated feliciter.';
+$lang['mariadb_username_invalid'] = 'Username est invalidum.';
+$lang['mariadb_hostname_invalid'] = 'Hostname est invalidum.';
+$lang['mariadb_password_invalid'] = 'Password irrita est.';
+$lang['mariadb_password_mismatch'] = 'Password cognoscere et non compositus.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Pone quaeso password database.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB PRAECLUSIO';
+$lang['mariadb_management_tool'] = 'Tool MariaDB Management';
+$lang['mariadb_management_tool_help'] = 'Sequere link to access the MariaDB procuratio tool. Si vos postulo muto vestri database tesserae Formam commodo.';
+$lang['mariadb_go_to_management_tool'] = 'Go To Mors MariaDB Database';
+$lang['mariadb_management_tool_not_accessible'] = 'In Database MariaDB tool est non available in in database, non currit,';
+$lang['mariadb_set_password'] = 'Set Password';
