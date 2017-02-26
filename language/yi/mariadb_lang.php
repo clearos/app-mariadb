7d6059490ee7c89c505bef1110aa7f4f03d34e72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:30:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'מאַריאַדב דייטאַבייס סערווער';
+$lang['mariadb_app_description'] = 'מאַריאַדב איז אַ פאָלקס דייטאַבייס סיסטעם. עס קענען זיין קאָנפיגורעד צו לויפן דייטאַבייס געטריבן אַפּלאַקיישאַנז, וועבסיטעס, קרם און פּראַקטאַקלי קיין אנדערע מיטל ריקוויירינג אַ ריליישאַנאַל סטאָרידזש דינסט.';
+$lang['mariadb_app_tooltip'] = 'דער שורש חשבון געניצט דורך די דייטאַבייס איז אַנדערש ווי די איין געניצט דורך די סיסטעם!';
+$lang['mariadb_mariadb_database'] = 'מאַריאַדב דאַטאַבאַסע';
+$lang['mariadb_access_denied'] = 'אַקסעס איז געלייקנט.';
+$lang['mariadb_password_updated'] = 'שפּריכוואָרט ופּדאַטעד הצלחה.';
+$lang['mariadb_username_invalid'] = 'נאמען איז פאַרקריפּלט.';
+$lang['mariadb_hostname_invalid'] = 'האָסטנאַמע איז פאַרקריפּלט.';
+$lang['mariadb_password_invalid'] = 'פּאַראָל איז פאַרקריפּלט.';
+$lang['mariadb_password_mismatch'] = 'שפּריכוואָרט און באַשטעטיקן טאָן ניט צופּאַסן.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ביטע שטעלן אַ דייטאַבייס פּאַראָל.';
+$lang['mariadb_mariadb_storage'] = 'מאַריאַדב סטאָרידזש';
+$lang['mariadb_management_tool'] = 'מאַריאַדב מאַנאַגעמענט טול';
+$lang['mariadb_management_tool_help'] = 'גיי די לינק צו צוטריט די מאַריאַדב פאַרוואַלטונג געצייַג. אויב איר דאַרפֿן צו טוישן אייער דייטאַבייס פּאַראָל, ביטע ניצן די פאָרעם אונטן.';
+$lang['mariadb_go_to_management_tool'] = 'גיין צו מאַריאַדב דאַטאַבאַסע מאַנאַגעמענט טול';
+$lang['mariadb_management_tool_not_accessible'] = 'די מאַריאַדב דאַטאַבאַסע מאַנאַגעמענט טול איז ניט בנימצא ווען די דאַטאַבאַסע איז נישט פליסנדיק';
+$lang['mariadb_set_password'] = 'שטעלן פּאַראָל';
