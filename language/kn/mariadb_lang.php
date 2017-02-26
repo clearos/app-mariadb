d2263297b606f9f258085e496ae3e3ed0bf00bd0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:24:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB ಡೇಟಾಬೇಸ್ ಸರ್ವರ್';
+$lang['mariadb_app_description'] = 'MariaDB ಜನಪ್ರಿಯ ಡೇಟಾಬೇಸ್ ವ್ಯವಸ್ಥೆ. ಇದು ಡೇಟಾಬೇಸ್ ಚಾಲಿತ ಅಪ್ಲಿಕೇಶನ್ಗಳು, ವೆಬ್ಸೈಟ್ಗಳು, CRM ಮತ್ತು ಪ್ರಾಯೋಗಿಕವಾಗಿ ಯಾವುದೇ ಇತರ ಸಂಪನ್ಮೂಲ ಸಂಬಂಧಕ ಶೇಖರಣಾ ಸೇವೆ ಅಗತ್ಯ ಚಲಾಯಿಸಲು ಸಂರಚಿಸಬಹುದು.';
+$lang['mariadb_app_tooltip'] = 'ಡೇಟಾಬೇಸ್ ಬಳಸುವ ಮೂಲ ಖಾತೆಯು ವ್ಯವಸ್ಥೆಯು ಬಳಸುವ ಒಂದು ಭಿನ್ನವಾದುದಾಗಿದೆ!';
+$lang['mariadb_mariadb_database'] = 'MariaDB ಡೇಟಾಬೇಸ್';
+$lang['mariadb_access_denied'] = 'ಪ್ರವೇಶ ನಿರಾಕರಿಸಲಾಗಿದೆ.';
+$lang['mariadb_password_updated'] = 'ಪಾಸ್ವರ್ಡ್ ಯಶಸ್ವಿಯಾಗಿ ಅಪ್ಡೇಟ್ಗೊಳಿಸಲಾಗಿದೆ.';
+$lang['mariadb_username_invalid'] = 'ಬಳಕೆದಾರ ಹೆಸರು ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mariadb_hostname_invalid'] = 'ಆತಿಥೇಯ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mariadb_password_invalid'] = 'ಪಾಸ್ವರ್ಡ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mariadb_password_mismatch'] = 'ಪಾಸ್ವರ್ಡ್ ಮತ್ತು ಪರಿಶೀಲಿಸಲು ಹೊಂದುತ್ತಿಲ್ಲ.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ದಯವಿಟ್ಟು ಡೇಟಾಬೇಸ್ ಗುಪ್ತಪದವನ್ನು.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB ಶೇಖರಣಾ';
+$lang['mariadb_management_tool'] = 'MariaDB ಮ್ಯಾನೇಜ್ಮೆಂಟ್ ಟೂಲ್';
+$lang['mariadb_management_tool_help'] = 'MariaDB ನಿರ್ವಹಣೆ ಸಾಧನ ಪ್ರವೇಶಿಸಲು ಲಿಂಕ್ ಅನುಸರಿಸಿ. ನಿಮ್ಮ ಡೇಟಾಬೇಸ್ ಗುಪ್ತಪದವನ್ನು ಬದಲಾಯಿಸಲು ಬಯಸಿದಲ್ಲಿ, ಕೆಳಗಿನ ರೂಪ ಬಳಸಿ.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB ಡೇಟಾಬೇಸ್ ಮ್ಯಾನೇಜ್ಮೆಂಟ್ ಟೂಲ್ ಹೋಗಿ';
+$lang['mariadb_management_tool_not_accessible'] = 'ಡೇಟಾಬೇಸ್ ಚಾಲನೆಯಲ್ಲಿಲ್ಲ ಮಾಡಿದಾಗ MariaDB ಡೇಟಾಬೇಸ್ ಮ್ಯಾನೇಜ್ಮೆಂಟ್ ಟೂಲ್ ಲಭ್ಯವಿಲ್ಲ';
+$lang['mariadb_set_password'] = 'ಪಾಸ್ವರ್ಡ್';
