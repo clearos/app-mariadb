20a9a1cddfbf3adc091030d46d31c63bc2979dd7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:25:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB ഡാറ്റാബേസ് സെർവർ';
+$lang['mariadb_app_description'] = 'MariaDB പ്രശസ്തമായ ഡാറ്റാബേസ് സിസ്റ്റം ആണ്. അതില് അപ്ലിക്കേഷനുകൾ, വെബ്സൈറ്റുകൾ CRM, ഒരു റിലേഷണൽ സംഭരണ ​​സേവനം ആവശ്യമില്ലാതെ പ്രായോഗികമായി മറ്റേതെങ്കിലും റിസോഴ്സ് ഡാറ്റാബേസ് പ്രവർത്തിപ്പിക്കുന്നതിന് ക്രമീകരിയ്ക്കാം.';
+$lang['mariadb_app_tooltip'] = 'ഡാറ്റാബേസ് ഉപയോഗിക്കുന്ന റൂട്ട് അക്കൌണ്ട് സിസ്റ്റം ഉപയോഗിക്കുന്ന ഒറ്റ വ്യത്യസ്തമാണ്!';
+$lang['mariadb_mariadb_database'] = 'MariaDB ഡാറ്റാബേസ്';
+$lang['mariadb_access_denied'] = 'പ്രവേശനം നിഷിദ്ധം.';
+$lang['mariadb_password_updated'] = 'പാസ്വേഡ് വിജയകരമായി നവീകരിച്ചു.';
+$lang['mariadb_username_invalid'] = 'ഉപയോക്തൃനാമം അസാധുവാണ്.';
+$lang['mariadb_hostname_invalid'] = 'ഹോസ്റ്റ്നാമം അസാധുവാണ്.';
+$lang['mariadb_password_invalid'] = 'പാസ്വേഡ് അസാധുവാണ്.';
+$lang['mariadb_password_mismatch'] = 'യോജിക്കുന്നില്ല പാസ്വേഡ് പരിശോധിക്കുകയും.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ഒരു ഡാറ്റാബേസ് പാസ്വേഡ് സജ്ജമാക്കുക.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB സംഭരണം';
+$lang['mariadb_management_tool'] = 'MariaDB മാനേജ്മെന്റ് ടൂൾ';
+$lang['mariadb_management_tool_help'] = 'MariaDB മാനേജ്മെന്റ് ഉപകരണം ആക്സസ് ലിങ്ക് പിന്തുടരുക. നിങ്ങളുടെ ഡാറ്റാബേസ് പാസ്വേഡ് മാറ്റാൻ വേണമെങ്കിൽ, താഴെയുള്ള ഫോം ഉപയോഗിക്കുക.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB ഡാറ്റാബേസ് മാനേജ്മെന്റ് ടൂൾ പോകുക';
+$lang['mariadb_management_tool_not_accessible'] = 'ഡാറ്റാബേസ് പ്രവർത്തിക്കുന്നില്ല വരുമ്പോൾ MariaDB ഡാറ്റാബേസ് മാനേജ്മെന്റ് ടൂൾ ലഭ്യമല്ല';
+$lang['mariadb_set_password'] = 'പാസ്വേഡ് സജ്ജമാക്കുക';
