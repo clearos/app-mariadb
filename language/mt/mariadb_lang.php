dd3c522ff3085fe0333f504b3e3fb8560b691497
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:26:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Server Database';
+$lang['mariadb_app_description'] = 'MariaDB hija sistema tad-database popolari. Dan jista &#39;jiġi kkonfigurat biex jużaw applikazzjonijiet database mmexxija, websajts, CRM u prattikament kull riżorsa oħra li jeħtieġu servizz ħażna relazzjonali.';
+$lang['mariadb_app_tooltip'] = 'Il-kont għerq użata mill-database hija differenti minn dik użata mis-sistema!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Aċċess huwa miċħud.';
+$lang['mariadb_password_updated'] = 'Password aġġornata b&#39;suċċess.';
+$lang['mariadb_username_invalid'] = 'Username huwa invalidu.';
+$lang['mariadb_hostname_invalid'] = 'Hostname huwa invalidu.';
+$lang['mariadb_password_invalid'] = 'Password huwa invalidu.';
+$lang['mariadb_password_mismatch'] = 'Password u tivverifika ma jaqblux.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Jekk jogħġbok tistabbilixxi password database.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Ħażna';
+$lang['mariadb_management_tool'] = 'MariaDB Ġestjoni Għodda';
+$lang['mariadb_management_tool_help'] = 'Segwi l-link biex jaċċessaw il-għodda ta &#39;ġestjoni MariaDB. Jekk għandek bżonn għall-bidla password database tiegħek, jekk jogħġbok uża l-formola t&#39;hawn taħt.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB Għodda Database ta &#39;Ġestjoni';
+$lang['mariadb_management_tool_not_accessible'] = 'Il MariaDB Għodda Database ta &#39;Ġestjoni ma tkunx disponibbli meta d-database ma tkunx qed taħdem';
+$lang['mariadb_set_password'] = 'set Password';
