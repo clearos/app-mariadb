e248e23aaf70aabe7b5faefb93602e33c4cc10b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:28:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Server Database';
+$lang['mariadb_app_description'] = 'MariaDB waa nidaam database caan ah. Waa in la qaybiyay kartaa in uu ordo codsiyada database kexeeyey, websites, CRM iyo ficil ahaan khayraadka kale oo u baahan in adeeg kaydinta xidhiidh.';
+$lang['mariadb_app_tooltip'] = 'account The xididka loo isticmaalo by database ka duwan yahay mid ay isticmaali jireen nidaamka!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Access waa la diiday.';
+$lang['mariadb_password_updated'] = 'Password si guul ah updated.';
+$lang['mariadb_username_invalid'] = 'Username waa baadil.';
+$lang['mariadb_hostname_invalid'] = 'Hostname waa baadil.';
+$lang['mariadb_password_invalid'] = 'Password waa baadil.';
+$lang['mariadb_password_mismatch'] = 'Password iyo xaqiijiyo ma u dhigma.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Fadlan dhigay password database a.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Kaydinta';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Raac link si ay u helaan qalab ee maamulka MariaDB. Haddii aad u baahan tahay in aad bedesho password database, fadlan isticmaal foomka hoos ku qoran.';
+$lang['mariadb_go_to_management_tool'] = 'Tag Si MariaDB Management Database Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB Tool Management Database aan la heli karin marka database aan la ordaya';
+$lang['mariadb_set_password'] = 'Password Set';
