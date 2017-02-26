ab28b1f622ea462ad822a745f2ed639600a6f134
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:23:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB მონაცემთა ბაზის სერვერი';
+$lang['mariadb_app_description'] = 'MariaDB არის პოპულარული მონაცემთა სისტემა. ეს შეიძლება კონფიგურაცია გასაშვებად მონაცემთა ორიენტირებული პროგრამები, საიტები, CRM და პრაქტიკულად ნებისმიერი სხვა რესურსი მოითხოვს რელატიური შენახვის მომსახურება.';
+$lang['mariadb_app_tooltip'] = 'ძირეული ანგარიში მიერ მონაცემთა ბაზაში არის განსხვავებული, ვიდრე ერთი, რომელსაც სისტემა!';
+$lang['mariadb_mariadb_database'] = 'MariaDB მონაცემთა ბაზა';
+$lang['mariadb_access_denied'] = 'წვდომა არის უარყო.';
+$lang['mariadb_password_updated'] = 'პაროლი წარმატებით განახლდა.';
+$lang['mariadb_username_invalid'] = 'მომხმარებლის სახელი არასწორია.';
+$lang['mariadb_hostname_invalid'] = 'ჰოსტის არასწორია.';
+$lang['mariadb_password_invalid'] = 'პაროლი არასწორია.';
+$lang['mariadb_password_mismatch'] = 'პაროლი და გადამოწმების არ ემთხვევა.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'გთხოვთ, მითითებული მონაცემთა ბაზის პაროლი.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB მართვის Tool';
+$lang['mariadb_management_tool_help'] = 'ბმულზე წვდომის MariaDB მართვის ინსტრუმენტი. თუ თქვენ უნდა შეცვალოთ თქვენი მონაცემთა ბაზის პაროლი, გთხოვთ გამოიყენოთ ქვემოთ მოცემული ფორმა.';
+$lang['mariadb_go_to_management_tool'] = 'გადასვლა MariaDB მონაცემთა ბაზის მართვის ინსტრუმენტი';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB მონაცემთა ბაზის მართვის ინსტრუმენტი არის შესაძლებელი, როდესაც მონაცემთა ბაზაში არ არის გაშვებული';
+$lang['mariadb_set_password'] = 'პაროლის დაყენება';
