9545292b546269600d814c09cbef4a5c31135040
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:20:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB的数据库服务器';
+$lang['mariadb_app_description'] = 'MariaDB的是一个流行的数据库系统。 它可以被配置为运行数据库驱动的应用程序，网站，CRM和几乎任何其它资源需要关系存储服务。';
+$lang['mariadb_app_tooltip'] = '数据库使用root帐户是不是由系统所使用的不同！';
+$lang['mariadb_mariadb_database'] = 'MariaDB的数据库';
+$lang['mariadb_access_denied'] = '访问被拒绝。';
+$lang['mariadb_password_updated'] = '密码已成功更新。';
+$lang['mariadb_username_invalid'] = '无效的用户名。';
+$lang['mariadb_hostname_invalid'] = '主机名是无效的。';
+$lang['mariadb_password_invalid'] = '密码是无效的。';
+$lang['mariadb_password_mismatch'] = '密码并确认不匹配。';
+$lang['mariadb_lang_please_set_a_database_password'] = '请设置数据库密码。';
+$lang['mariadb_mariadb_storage'] = 'MariaDB的存储';
+$lang['mariadb_management_tool'] = 'MariaDB的管理工具';
+$lang['mariadb_management_tool_help'] = '按照访问MariaDB的管理工具的链接。 如果您需要更改数据库密码，请使用下表。';
+$lang['mariadb_go_to_management_tool'] = '转到MariaDB的数据库管理工具';
+$lang['mariadb_management_tool_not_accessible'] = '该MariaDB的数据库管理工具是不可用时，数据库没有运行';
+$lang['mariadb_set_password'] = '设置密码';
