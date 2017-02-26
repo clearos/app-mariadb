b39a0edab481c0d7c9d30ad9952253200e706ef6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:31:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB aaye data Server';
+$lang['mariadb_app_description'] = 'MariaDB ni a gbajumo database eto. O le wa ni tunto lati ṣiṣe database lé ohun elo, wẹbusaiti, CRM ati Oba eyikeyi miiran awọn oluşewadi nilo a ti ibatan ipamọ iṣẹ.';
+$lang['mariadb_app_tooltip'] = 'Awọn root iroyin lo nipasẹ awọn database ti o yatọ si ju ọkan lo nipasẹ awọn eto!';
+$lang['mariadb_mariadb_database'] = 'MariaDB aaye data';
+$lang['mariadb_access_denied'] = 'Access ti wa ni sẹ.';
+$lang['mariadb_password_updated'] = 'Ọrọigbaniwọle imudojuiwọn ni ifijišẹ.';
+$lang['mariadb_username_invalid'] = 'Orukọ olumulo ti wa ni invalid.';
+$lang['mariadb_hostname_invalid'] = 'Hostname jẹ invalid.';
+$lang['mariadb_password_invalid'] = 'Aṣínà ni invalid.';
+$lang['mariadb_password_mismatch'] = 'Ọrọigbaniwọle ki o si mọ daju ko baramu.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Jọwọ ṣeto a database igbaniwọle.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Ibi';
+$lang['mariadb_management_tool'] = 'MariaDB Management ọpa';
+$lang['mariadb_management_tool_help'] = 'Tẹle awọn ọna asopọ lati wọle si awọn MariaDB isakoso ọpa. Ti o ba nilo lati yi rẹ database ọrọigbaniwọle, jọwọ lo awọn fọọmu ni isalẹ.';
+$lang['mariadb_go_to_management_tool'] = 'Lọ Lati MariaDB aaye data Management ọpa';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB aaye data Management ọpa ni ko wa nigbati awọn database ti wa ni ko nṣiṣẹ';
+$lang['mariadb_set_password'] = 'ṣeto Ọrọigbaniwọle';
