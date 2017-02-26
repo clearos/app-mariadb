18d8d2397faa16fc4fb61f78a4a9b33e57df0cc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:26:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Pangkalan Data Server';
+$lang['mariadb_app_description'] = 'MariaDB adalah satu sistem pangkalan data popular. Ia boleh dikonfigurasikan untuk menjalankan pangkalan data didorong aplikasi, laman web, CRM dan praktikal mana-mana sumber lain yang memerlukan perkhidmatan simpanan hubungan.';
+$lang['mariadb_app_tooltip'] = 'Akaun akar yang digunakan oleh pangkalan data adalah berbeza daripada yang digunakan oleh sistem!';
+$lang['mariadb_mariadb_database'] = 'Pangkalan MariaDB';
+$lang['mariadb_access_denied'] = 'Akses tidak dibenarkan.';
+$lang['mariadb_password_updated'] = 'Kata laluan berjaya dikemas kini.';
+$lang['mariadb_username_invalid'] = 'Nama pengguna tidak sah.';
+$lang['mariadb_hostname_invalid'] = 'Nama hos tidak sah.';
+$lang['mariadb_password_invalid'] = 'Kata laluan tidak sah.';
+$lang['mariadb_password_mismatch'] = 'Kata laluan dan mengesahkan tidak sepadan.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Sila tetapkan kata laluan pangkalan data.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'Alat Pengurusan MariaDB';
+$lang['mariadb_management_tool_help'] = 'Ikut pautan untuk mengakses alat pengurusan MariaDB. Jika anda perlu menukar kata laluan pangkalan data anda, sila gunakan borang di bawah.';
+$lang['mariadb_go_to_management_tool'] = 'Pergi Untuk MariaDB Alat Pengurusan Pangkalan Data';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB Alat Pengurusan Pangkalan Data tidak boleh didapati apabila pangkalan data tidak berjalan';
+$lang['mariadb_set_password'] = 'Tetapkan kata laluan';
