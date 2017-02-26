62a7e7c55e6356cfd1623c0e676cdafa7e3cd72f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:21:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Datenbank-Server';
+$lang['mariadb_app_description'] = 'MariaDB ist ein beliebtes Datenbanksystem. Es kann eine relationale Storage-Service-Datenbank-Applikationen, Webseiten, CRM und praktisch jede andere Ressource zu laufen erfordert konfiguriert werden.';
+$lang['mariadb_app_tooltip'] = 'Der Root-Account von der Datenbank verwendet wird, ist anders als die eine von dem System verwendet!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Datenbank';
+$lang['mariadb_access_denied'] = 'Zugriff wird verweigert.';
+$lang['mariadb_password_updated'] = 'Kennwort erfolgreich aktualisiert.';
+$lang['mariadb_username_invalid'] = 'Benutzername ist ungültig.';
+$lang['mariadb_hostname_invalid'] = 'Hostname ist ungültig.';
+$lang['mariadb_password_invalid'] = 'Passwort ist ungültig.';
+$lang['mariadb_password_mismatch'] = 'Kennwort und überprüfen Sie stimmen nicht überein.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Bitte setzen Sie ein Datenbank-Kennwort.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Lagerung';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Folgen Sie dem Link, um das MariaDB-Management-Tool zugreifen. Wenn Sie Ihre Datenbank-Kennwort ändern müssen, verwenden Sie bitte das folgende Formular aus.';
+$lang['mariadb_go_to_management_tool'] = 'Lesen Sie MariaDB Datenbank-Management-Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'Die MariaDB Datenbank-Management-Tool ist nicht verfügbar, wenn die Datenbank nicht läuft';
+$lang['mariadb_set_password'] = 'Passwort festlegen';
