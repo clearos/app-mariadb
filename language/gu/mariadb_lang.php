4922885b8b7cab0f16e9796e86b31b903af283de
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:22:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB એ ડેટાબેઝ સર્વર';
+$lang['mariadb_app_description'] = 'MariaDB એ એક લોકપ્રિય ડેટાબેઝ સિસ્ટમ છે. તે ડેટાબેઝ આધારિત કાર્યક્રમો, વેબસાઇટ્સ, CRM અને વ્યવહારીક કોઈપણ અન્ય સ્ત્રોત એક સંબંધ સંગ્રહ સેવા જરૂરી ચલાવવા માટે રૂપરેખાંકિત કરી શકાય છે.';
+$lang['mariadb_app_tooltip'] = 'રુટ ડેટાબેઝ દ્વારા ઉપયોગ એકાઉન્ટ સિસ્ટમ દ્વારા ઉપયોગ એક કરતાં અલગ છે!';
+$lang['mariadb_mariadb_database'] = 'MariaDB એ ડેટાબેઝ';
+$lang['mariadb_access_denied'] = 'પ્રવેશ નકાર્યો છે.';
+$lang['mariadb_password_updated'] = 'પાસવર્ડ સફળતાપૂર્વક સુધારાશે.';
+$lang['mariadb_username_invalid'] = 'વપરાશકર્તા નામ અમાન્ય છે.';
+$lang['mariadb_hostname_invalid'] = 'યજમાનનામ અમાન્ય છે.';
+$lang['mariadb_password_invalid'] = 'પાસવર્ડ અમાન્ય છે.';
+$lang['mariadb_password_mismatch'] = 'પાસવર્ડ અને ચકાસણી મેળ ખાતા નથી.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ડેટાબેઝ પાસવર્ડ સુયોજિત કરો.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB એ સંગ્રહ';
+$lang['mariadb_management_tool'] = 'MariaDB એ મેનેજમેન્ટ ટૂલ';
+$lang['mariadb_management_tool_help'] = 'MariaDB એ વ્યવસ્થાપન સાધન વાપરવા માટે લિંકને અનુસરો. તમે તમારા ડેટાબેઝ પાસવર્ડ બદલવા માટે જરૂર હોય તો, નીચે ફોર્મ ઉપયોગ કરો.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB એ ડેટાબેઝ મેનેજમેન્ટ ટૂલ જાઓ';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB એ ડેટાબેઝ મેનેજમેન્ટ ટૂલ ઉપલબ્ધ નથી જ્યારે ડેટાબેઝ ચાલી રહ્યું નથી';
+$lang['mariadb_set_password'] = 'સેટ પાસવર્ડ';
