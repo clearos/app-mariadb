ba2579151e5776da709bbabbea75b30dc7a3ea6e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:19:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB je popularan sistem baze podataka. To može biti konfiguriran za pokretanje baze podataka prešao aplikacije, web stranice, CRM i praktično bilo koji drugi resurs je potreban relacijskih servis za pohranu.';
+$lang['mariadb_app_tooltip'] = 'Root račun koristi baza podataka je različit od onog koji se koristi u sistemu!';
+$lang['mariadb_mariadb_database'] = 'MariaDB baza';
+$lang['mariadb_access_denied'] = 'Pristup odbijen.';
+$lang['mariadb_password_updated'] = 'Password uspješno ažuriran.';
+$lang['mariadb_username_invalid'] = 'Korisničko ime je nevažeći.';
+$lang['mariadb_hostname_invalid'] = 'Hostname je nevažeći.';
+$lang['mariadb_password_invalid'] = 'Lozinka je nevažeći.';
+$lang['mariadb_password_mismatch'] = 'Lozinke i provjerite ne podudaraju.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Molimo vas da postavite lozinku baze podataka.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Pratite link za pristup alat za upravljanje MariaDB. Ako je potrebno da promenite lozinku baze podataka, molimo vas da koristite obrazac ispod.';
+$lang['mariadb_go_to_management_tool'] = 'Idi na MariaDB alata za upravljanje bazama podataka';
+$lang['mariadb_management_tool_not_accessible'] = 'Alat za upravljanje bazama podataka je MariaDB nije dostupan kada se baza podataka ne radi';
+$lang['mariadb_set_password'] = 'set Password';
