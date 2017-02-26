37d709947f5e157911ab7b34e35e33b7d99c8659
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:29:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'Mariadb Veritabanı Sunucu';
+$lang['mariadb_app_description'] = 'Mariadb popüler bir veritabanı sistemidir. Ilişkisel depolama servisi gerektiren veritabanı yönelimli uygulamaları, web siteleri, CRM ve pratik başka bir kaynak çalıştırmak için yapılandırılabilir.';
+$lang['mariadb_app_tooltip'] = 'veritabanı tarafından kullanılan kök hesap sistemi tarafından kullanılan olandan farklı!';
+$lang['mariadb_mariadb_database'] = 'mariadb Veritabanı';
+$lang['mariadb_access_denied'] = 'Giriş reddedildi.';
+$lang['mariadb_password_updated'] = 'Şifre başarıyla güncellendi.';
+$lang['mariadb_username_invalid'] = 'Kullanıcı adı geçersiz.';
+$lang['mariadb_hostname_invalid'] = 'Ana makine adı geçersiz.';
+$lang['mariadb_password_invalid'] = 'Şifre geçersiz.';
+$lang['mariadb_password_mismatch'] = 'Şifre ve eşleşmiyor doğrulayın.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Bir veritabanı şifrenizi giriniz.';
+$lang['mariadb_mariadb_storage'] = 'mariadb Depolama';
+$lang['mariadb_management_tool'] = 'Mariadb Yönetim Aracı';
+$lang['mariadb_management_tool_help'] = 'Mariadb yönetim aracı erişmek için bağlantıyı takip edin. Eğer veritabanı parolası değiştirmeniz gerekiyorsa, lütfen aşağıdaki formu kullanınız.';
+$lang['mariadb_go_to_management_tool'] = 'Mariadb Veritabanı Yönetim Aracı Git';
+$lang['mariadb_management_tool_not_accessible'] = 'Veritabanı çalışmadığı zaman mariadb Veritabanı Yönetim Aracı mevcut değildir';
+$lang['mariadb_set_password'] = 'Şifreyi belirle';
