aaa6c0b4c74dcc7e505f21fee414ebde2824632a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:27:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB විසින් දත්ත සමුදා සේවාදායකය';
+$lang['mariadb_app_description'] = 'MariaDB ජනප්රිය දත්ත සමුදා පද්ධතියට වේ. එය යෙදුම්, වෙබ් අඩවි, සී.ආර්.එම් සහ සම්බන්ධක ගබඩා සේවා අවශ්ය ප්රායෝගිකව ඕනෑම අනෙකුත් සම්පත් ධාවනය දත්ත සමුදාය ක්රියාත්මක කිරීමට මානකල හැක.';
+$lang['mariadb_app_tooltip'] = 'දත්ත සමුදාය විසින් භාවිතා root ගිණුමක් පද්ධතිය විසින් භාවිතා කරන එක් කෙනෙකුට වඩා වෙනස් වේ!';
+$lang['mariadb_mariadb_database'] = 'MariaDB දත්ත';
+$lang['mariadb_access_denied'] = 'ඇතුළුවීම තහනම්.';
+$lang['mariadb_password_updated'] = 'මුර-පදය සාර්ථක ලෙස යාවත්කාලීන කරන ලදී.';
+$lang['mariadb_username_invalid'] = 'පරිශීලක නාමය වලංගු නැත.';
+$lang['mariadb_hostname_invalid'] = 'ධාරක නාමය වලංගු නැත.';
+$lang['mariadb_password_invalid'] = 'මුරපදය වලංගු නොවේ.';
+$lang['mariadb_password_mismatch'] = 'නොගැලපේ මුරපදය හා තහවුරු.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'දත්ත සමුදා රහස්පදය ඇතුළත් කරන්න.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB ගබඞා';
+$lang['mariadb_management_tool'] = 'MariaDB කළමනාකරණ පද්ධතියක්';
+$lang['mariadb_management_tool_help'] = 'මෙම MariaDB කළමනාකරණ මෙවලමක් ප්රවේශ සබැඳිය අනුගමනය කරන්න. , ඔබ විසින් දත්ත සමුදා මුරපදය වෙනස් කිරීමට අවශ්ය නම්, පහත ආකෘති පත්රය භාවිතා කරන්න.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB දත්ත කළමනාකරණ පද්ධතියක් වෙත යන්න';
+$lang['mariadb_management_tool_not_accessible'] = 'දත්ත සමුදාය ක්රියාත්මක නොවේ විට MariaDB දත්ත කළමනාකරණ පද්ධතියක් නොමැති';
+$lang['mariadb_set_password'] = 'සකසන්න මුරපදය';
