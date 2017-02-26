3284a87b4cfcd7c4ce7e37b91c77e8ecc5e1f491
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:22:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB adatbázis szerver';
+$lang['mariadb_app_description'] = 'MariaDB egy népszerű adatbázis-rendszer. Azt lehet beállítani, hogy fut adatbázis alapú alkalmazások, weboldalak, CRM és gyakorlatilag bármilyen más forrás igénylő relációs tárolási szolgáltatást.';
+$lang['mariadb_app_tooltip'] = 'A root fiók által használt adatbázis más, mint az, amelyet a rendszer!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Hozzáférés megtagadva.';
+$lang['mariadb_password_updated'] = 'Jelszó módosítva.';
+$lang['mariadb_username_invalid'] = 'Érvénytelen felhasználónév.';
+$lang['mariadb_hostname_invalid'] = 'Hostname érvénytelen.';
+$lang['mariadb_password_invalid'] = 'Jelszó érvénytelen.';
+$lang['mariadb_password_mismatch'] = 'Jelszó és ellenőrizze nem egyezik.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Kérjük, állítsa be az adatbázis jelszavát.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB tárolás';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Kövesse a linket, amellyel a MariaDB eszköz. Ha meg kell változtatni az adatbázis jelszavát, kérjük használja az alábbi űrlapot.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB adatbázis-kezelő eszköz';
+$lang['mariadb_management_tool_not_accessible'] = 'A MariaDB adatbázis-kezelő eszköz nem használható, ha az adatbázis nem fut';
+$lang['mariadb_set_password'] = 'Állítsd be a jelszót';
