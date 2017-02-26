0c016fa23598dbfa748b143c21601bc62fd37935
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:26:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'ماریادیبی پایگاه داده و سرور';
+$lang['mariadb_app_description'] = 'ماریادیبی یک سیستم پایگاه داده محبوب است. می توان آن را پیکربندی شده برای اجرای برنامه های کاربردی پایگاه داده محور، وب سایت، CRM و عملا هر منبع دیگر نیاز به یک سرویس ذخیره سازی رابطه ای است.';
+$lang['mariadb_app_tooltip'] = 'حساب ریشه استفاده شده توسط پایگاه داده از یک مورد استفاده توسط سیستم متفاوت است!';
+$lang['mariadb_mariadb_database'] = 'پایگاه ماریادیبی';
+$lang['mariadb_access_denied'] = 'دسترسی است.';
+$lang['mariadb_password_updated'] = 'رمز عبور با موفقیت به روز شد.';
+$lang['mariadb_username_invalid'] = 'نام کاربری نامعتبر است.';
+$lang['mariadb_hostname_invalid'] = 'نام میزبان نامعتبر است.';
+$lang['mariadb_password_invalid'] = 'رمز عبور نادرست است.';
+$lang['mariadb_password_mismatch'] = 'رمز عبور و بررسی مطابقت ندارند.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'لطفا رمز عبور پایگاه داده تنظیم شده است.';
+$lang['mariadb_mariadb_storage'] = 'ماریادیبی ذخیره سازی';
+$lang['mariadb_management_tool'] = 'ابزار مدیریت ماریادیبی';
+$lang['mariadb_management_tool_help'] = 'دنبال لینک برای دسترسی به ابزار مدیریت ماریادیبی. اگر شما نیاز به تغییر رمز عبور پایگاه داده خود، لطفا فرم زیر استفاده کنید.';
+$lang['mariadb_go_to_management_tool'] = 'برو به ماریادیبی ابزار مدیریت پایگاه داده';
+$lang['mariadb_management_tool_not_accessible'] = 'ابزار مدیریت پایگاه ماریادیبی در دسترس نیست زمانی که پایگاه داده در حال اجرا نیست';
+$lang['mariadb_set_password'] = 'قراردادن رمز عبور';
