b5bff68e015fa155c4f15b39dc53372f8904a66d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:21:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB on populaarne andmebaasi süsteemi. See võib olla konfigureeritud andmebaasi sõita rakendusi, veebisaite, CRM ja praktiliselt kõik muud ressurssi nõudvate suhtlev ladustamise teenust.';
+$lang['mariadb_app_tooltip'] = 'Root kasutab andmebaas on teistsugune kui see, mida kasutatakse süsteemi!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Juurdepääs on keelatud.';
+$lang['mariadb_password_updated'] = 'Salasõna edukalt uuendatud.';
+$lang['mariadb_username_invalid'] = 'Kasutaja on kehtetu.';
+$lang['mariadb_hostname_invalid'] = 'Masinanimi ei sobi.';
+$lang['mariadb_password_invalid'] = 'Parool on vale.';
+$lang['mariadb_password_mismatch'] = 'Salasõna ja kontrollida ei sobi.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Palun määra andmebaasi parool.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB juhtimise tööriist';
+$lang['mariadb_management_tool_help'] = 'Jälgi lingil MariaDB juhtimisvahend. Kui teil on vaja, et muuta oma andmebaasi parooli, siis kasutage allpool olevat vormi.';
+$lang['mariadb_go_to_management_tool'] = 'Mine MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB Database Management Tool ei ole kättesaadav kui andmebaas ei tööta';
+$lang['mariadb_set_password'] = 'Set Password';
