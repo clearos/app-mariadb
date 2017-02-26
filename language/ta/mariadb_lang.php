bfc00f397d65d9c474d63dd260c5bea228ab34fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:29:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB, டேட்டாபேஸ் சர்வர்';
+$lang['mariadb_app_description'] = 'MariaDB, ஒரு பிரபலமான தரவுத்தள அமைப்பு. அது தரவுத்தள இயங்கும் பயன்பாடுகள், இணையதளங்கள், CRM, மற்றும் நடைமுறையில் மற்ற எந்த வள தொடர்புடைய சேமிப்பு சேவை தேவைப்படும் இயக்க கட்டமைக்க முடியும்.';
+$lang['mariadb_app_tooltip'] = 'தகவல் பயன்படுத்தப்படும் ரூட் கணக்கு அமைப்பு பயன்படுத்தப்படும் ஒரு விட வித்தியாசமாக இருக்கும்!';
+$lang['mariadb_mariadb_database'] = 'MariaDB, டேட்டாபேஸ்';
+$lang['mariadb_access_denied'] = 'நுழைவு மறுக்கபடுகிறது.';
+$lang['mariadb_password_updated'] = 'கடவுச்சொல் வெற்றிகரமாக மேம்படுத்தப்பட்டது.';
+$lang['mariadb_username_invalid'] = 'பயனர்பெயர் தவறானது.';
+$lang['mariadb_hostname_invalid'] = 'பெயர் தவறானது.';
+$lang['mariadb_password_invalid'] = 'கடவுச்சொல் தவறானது.';
+$lang['mariadb_password_mismatch'] = 'கடவுச்சொல் மற்றும் சரிபார்க்க பொருந்தவில்லை.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ஒரு தகவல் கடவுச்சொல்லை அமைக்க கொள்ளவும்.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB, சேமிப்பு';
+$lang['mariadb_management_tool'] = 'MariaDB, மேலாண்மை கருவி';
+$lang['mariadb_management_tool_help'] = 'MariaDB, மேலாண்மை கருவியை அணுக இணைப்பை பின்பற்றவும். நீங்கள் உங்கள் தரவுத்தள கடவுச்சொல்லை மாற்ற வேண்டும் என்றால், கீழே உள்ள படிவத்தை பயன்படுத்தவும்.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB, தரவுத்தள மேலாண்மை கருவி சென்று';
+$lang['mariadb_management_tool_not_accessible'] = 'தகவல் இயங்காத நிலையில் அந்த MariaDB, தரவுத்தள மேலாண்மை கருவி இல்லை';
+$lang['mariadb_set_password'] = 'கடவுச்சொல் அமை';
