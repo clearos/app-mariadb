ae11d03e6683ba55d11075bc8ab15e24af469eae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:21:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB on suosittu tietokantajärjestelmän. Se voidaan konfiguroida toimimaan tietokantaan perustuvia sovelluksia, sivustot, CRM ja käytännössä kaikki muut resurssi vaatii relaatio varastointi palvelu.';
+$lang['mariadb_app_tooltip'] = 'Root-tunnuksen käyttämä tietokanta on erilainen kuin se, jota käytetään järjestelmän!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Pääsy kielletty.';
+$lang['mariadb_password_updated'] = 'Salasana päivitetty.';
+$lang['mariadb_username_invalid'] = 'Käyttäjätunnus on virheellinen.';
+$lang['mariadb_hostname_invalid'] = 'Hostname on virheellinen.';
+$lang['mariadb_password_invalid'] = 'Salasana ei kelpaa.';
+$lang['mariadb_password_mismatch'] = 'Salasana ja tarkistaa eivät täsmää.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Aseta tietokannan salasana.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB hallintatyökalu';
+$lang['mariadb_management_tool_help'] = 'Seuraa linkin kautta MariaDB hallintatyökalu. Jos haluat vaihtaa tietokannan salasanaa, käytä alla olevaa lomaketta.';
+$lang['mariadb_go_to_management_tool'] = 'Siirry MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB Database Management Tool ei ole käytettävissä, kun tietokanta ei ole käynnissä';
+$lang['mariadb_set_password'] = 'Aseta salasana';
