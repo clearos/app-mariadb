09a3dfa06333bd8db2c849e7c1bed9c72ecd838c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:26:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB ਡਾਟਾਬੇਸ ਸਰਵਰ';
+$lang['mariadb_app_description'] = 'MariaDB ਇੱਕ ਪ੍ਰਸਿੱਧ ਡਾਟਾਬੇਸ ਸਿਸਟਮ ਹੈ. ਇਹ ਡੇਟਾਬੇਸ ਚਲਾਏ ਕਾਰਜ, ਵੈੱਬਸਾਈਟ, CRM ਅਤੇ ਲਗਭਗ ਹੋਰ ਕਿਸੇ ਵੀ ਸਰੋਤ ਨੂੰ ਇੱਕ ਰਿਲੇਸ਼ਨਲ ਸਟੋਰੇਜ਼ ਸੇਵਾ ਦੀ ਲੋੜ ਹੈ ਨੂੰ ਚਲਾਉਣ ਲਈ ਸੰਰਚਿਤ ਕੀਤਾ ਜਾ ਸਕਦਾ ਹੈ.';
+$lang['mariadb_app_tooltip'] = 'ਰੂਟ ਡਾਟਾਬੇਸ ਦੁਆਰਾ ਵਰਤਿਆ ਖਾਤੇ ਸਿਸਟਮ ਦੁਆਰਾ ਵਰਤਿਆ ਇੱਕ ਵੱਧ ਵੱਖ ਵੱਖ ਹੁੰਦਾ ਹੈ!';
+$lang['mariadb_mariadb_database'] = 'MariaDB ਡਾਟਾਬੇਸ';
+$lang['mariadb_access_denied'] = 'ਪਹੁੰਚ ਪਾਬੰਦੀ ਹੈ ਹੈ.';
+$lang['mariadb_password_updated'] = 'ਪਾਸਵਰਡ ਸਫਲਤਾਪੂਰਕ ਅੱਪਡੇਟ ਕੀਤਾ.';
+$lang['mariadb_username_invalid'] = 'ਯੂਜ਼ਰ ਗਲਤ ਹੈ.';
+$lang['mariadb_hostname_invalid'] = 'ਹੋਸਟ ਗਲਤ ਹੈ.';
+$lang['mariadb_password_invalid'] = 'ਪਾਸਵਰਡ ਗਲਤ ਹੈ.';
+$lang['mariadb_password_mismatch'] = 'ਪਾਸਵਰਡ ਅਤੇ ਤਸਦੀਕ ਮੇਲ ਨਾ ਕਰੋ.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ਨੂੰ ਇੱਕ ਡਾਟਾਬੇਸ &#39;ਪਾਸਵਰਡ ਸੈੱਟ ਕਰੋ ਜੀ.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB ਸਟੋਰੇਜ਼';
+$lang['mariadb_management_tool'] = 'MariaDB ਪਰਬੰਧਨ ਸੰਦ';
+$lang['mariadb_management_tool_help'] = 'MariaDB ਪ੍ਰਬੰਧਨ ਟੂਲ ਨੂੰ ਪਹੁੰਚ ਕਰਨ ਲਈ ਲਿੰਕ ਨੂੰ ਦੀ ਪਾਲਣਾ ਕਰੋ. ਤੁਹਾਨੂੰ ਆਪਣੇ ਡਾਟਾਬੇਸ ਨੂੰ ਪਾਸਵਰਡ ਤਬਦੀਲ ਕਰਨ ਦੀ ਲੋੜ ਹੈ, ਜੇ, ਹੇਠ ਦਿੱਤੇ ਫਾਰਮ ਨੂੰ ਵਰਤ ਕਰੋ ਜੀ.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB ਡਾਟਾਬੇਸ ਪਰਬੰਧਨ ਸੰਦ ਲਈ ਜਾਓ';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB ਡਾਟਾਬੇਸ ਪਰਬੰਧਨ ਸੰਦ ਉਪਲੱਬਧ ਨਹੀ ਹੈ, ਜਦ ਕਿ ਡਾਟਾਬੇਸ ਹੈ, ਨਾ ਚੱਲ ਰਿਹਾ ਹੈ';
+$lang['mariadb_set_password'] = 'ਸੈੱਟ ਪਾਸਵਰਡ';
