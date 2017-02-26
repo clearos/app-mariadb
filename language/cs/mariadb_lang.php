edc4b24c13ea5b33e10b282987fc873eabae92e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:20:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'Mariadb Databázový server';
+$lang['mariadb_app_description'] = 'Mariadb je populární databázový systém. To může být nakonfigurován pro spuštění databáze řízené aplikace, webové stránky, CRM a prakticky jakýkoli jiný zdroj, vyžadující relační zavazadel.';
+$lang['mariadb_app_tooltip'] = 'Účet root používá databázi je jiný než ten, používaného systému!';
+$lang['mariadb_mariadb_database'] = 'mariadb Database';
+$lang['mariadb_access_denied'] = 'Přístup odepřen.';
+$lang['mariadb_password_updated'] = 'Heslo úspěšně aktualizován.';
+$lang['mariadb_username_invalid'] = 'Uživatelské jméno je neplatné.';
+$lang['mariadb_hostname_invalid'] = 'Hostname je neplatný.';
+$lang['mariadb_password_invalid'] = 'Heslo je neplatné.';
+$lang['mariadb_password_mismatch'] = 'Heslem a ověřit neshodují.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Prosím nastavit heslo databáze.';
+$lang['mariadb_mariadb_storage'] = 'mariadb Storage';
+$lang['mariadb_management_tool'] = 'Mariadb Management Tool';
+$lang['mariadb_management_tool_help'] = 'Klikněte na odkaz pro přístup nástroj pro správu mariadb. Pokud potřebujete změnit heslo databáze, použijte níže uvedený formulář.';
+$lang['mariadb_go_to_management_tool'] = 'Přejít na mariadb nástroj pro správu databáze';
+$lang['mariadb_management_tool_not_accessible'] = 'Nástroj Správa databáze mariadb není k dispozici, pokud je databáze neběží';
+$lang['mariadb_set_password'] = 'Nastavit heslo';
