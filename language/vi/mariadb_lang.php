1c609d5ee09572bd761b8d3eeca6982e09eed763
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:30:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB là một hệ thống cơ sở dữ liệu phổ biến. Nó có thể được cấu hình để chạy cơ sở dữ liệu hướng ứng dụng, trang web, CRM và thực tế tài nguyên nào khác đòi hỏi một dịch vụ lưu trữ quan hệ.';
+$lang['mariadb_app_tooltip'] = 'Các tài khoản gốc được sử dụng bởi cơ sở dữ liệu là khác nhau hơn so với sử dụng bởi hệ thống!';
+$lang['mariadb_mariadb_database'] = 'Cơ sở dữ liệu MariaDB';
+$lang['mariadb_access_denied'] = 'Truy cập bị từ chối.';
+$lang['mariadb_password_updated'] = 'Mật khẩu được cập nhật thành công.';
+$lang['mariadb_username_invalid'] = 'Tên đăng nhập không hợp lệ.';
+$lang['mariadb_hostname_invalid'] = 'Hostname là không hợp lệ.';
+$lang['mariadb_password_invalid'] = 'Mật khẩu không đúng.';
+$lang['mariadb_password_mismatch'] = 'Mật khẩu và xác nhận không khớp.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Hãy thiết lập một mật khẩu cơ sở dữ liệu.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB lưu trữ';
+$lang['mariadb_management_tool'] = 'Công cụ quản lý MariaDB';
+$lang['mariadb_management_tool_help'] = 'Thực hiện theo các liên kết để truy cập vào các công cụ quản lý MariaDB. Nếu bạn cần thay đổi mật khẩu cơ sở dữ liệu của bạn, xin vui lòng sử dụng mẫu dưới đây.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB Công cụ quản lý cơ sở dữ liệu';
+$lang['mariadb_management_tool_not_accessible'] = 'Các MariaDB Công cụ quản lý cơ sở dữ liệu không có sẵn khi cơ sở dữ liệu không phải là chạy';
+$lang['mariadb_set_password'] = 'Đặt mật khẩu';
