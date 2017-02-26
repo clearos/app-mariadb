28425975fbe3cd8481ee5184190ef6e7ac59641e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:26:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB er et populært databasesystem. Den kan konfigureres til å kjøre databasedrevne applikasjoner, nettsteder, CRM og praktisk talt alle andre ressurs som krever en relasjonsoppbevaring.';
+$lang['mariadb_app_tooltip'] = 'Root kontoen brukes av databasen er annerledes enn den som brukes av systemet!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Ingen tilgang.';
+$lang['mariadb_password_updated'] = 'Passord oppdatert.';
+$lang['mariadb_username_invalid'] = 'Brukernavnet er ugyldig.';
+$lang['mariadb_hostname_invalid'] = 'Vertsnavn er ugyldig.';
+$lang['mariadb_password_invalid'] = 'Passordet er ugyldig.';
+$lang['mariadb_password_mismatch'] = 'Passord og bekrefte samsvarer ikke.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Vennligst angi et databasepassord.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Følg linken for å få tilgang til MariaDB styringsverktøy. Hvis du trenger å endre databasepassord, kan du bruke skjemaet under.';
+$lang['mariadb_go_to_management_tool'] = 'Gå til MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'Den MariaDB Database Management Tool er ikke tilgjengelig når databasen ikke er i gang';
+$lang['mariadb_set_password'] = 'Lag et passord';
