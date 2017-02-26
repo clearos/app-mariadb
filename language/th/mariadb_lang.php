327a46ea3b354e8ae82a9a44465aa34264e54516
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:29:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB เซิร์ฟเวอร์ฐานข้อมูล';
+$lang['mariadb_app_description'] = 'MariaDB เป็นระบบฐานข้อมูลที่นิยม จะสามารถกำหนดค่าในการใช้งานฐานข้อมูลขับเคลื่อน, เว็บไซต์, CRM และในทางปฏิบัติทรัพยากรอื่น ๆ ที่ต้องใช้บริการจัดเก็บสัมพันธ์';
+$lang['mariadb_app_tooltip'] = 'บัญชี root โดยใช้ฐานข้อมูลที่แตกต่างกันมากกว่าหนึ่งที่ใช้โดยระบบ!';
+$lang['mariadb_mariadb_database'] = 'ฐานข้อมูล MariaDB';
+$lang['mariadb_access_denied'] = 'การเข้าถึงถูกปฏิเสธ.';
+$lang['mariadb_password_updated'] = 'รหัสผ่านการปรับปรุงเรียบร้อยแล้ว';
+$lang['mariadb_username_invalid'] = 'ชื่อผู้ใช้ไม่ถูกต้อง';
+$lang['mariadb_hostname_invalid'] = 'ชื่อโฮสต์ไม่ถูกต้อง';
+$lang['mariadb_password_invalid'] = 'รหัสผ่านไม่ถูกต้อง';
+$lang['mariadb_password_mismatch'] = 'รหัสผ่านและการตรวจสอบไม่ตรงกับ';
+$lang['mariadb_lang_please_set_a_database_password'] = 'กรุณาตั้งค่ารหัสผ่านฐานข้อมูล';
+$lang['mariadb_mariadb_storage'] = 'การจัดเก็บ MariaDB';
+$lang['mariadb_management_tool'] = 'เครื่องมือการจัดการ MariaDB';
+$lang['mariadb_management_tool_help'] = 'ตามลิงค์นี้เพื่อเข้าถึงเครื่องมือการจัดการ MariaDB หากคุณจำเป็นต้องเปลี่ยนรหัสผ่านฐานข้อมูลของคุณโปรดใช้แบบฟอร์มด้านล่าง';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB เครื่องมือการจัดการฐานข้อมูล';
+$lang['mariadb_management_tool_not_accessible'] = 'เครื่องมือการจัดการฐานข้อมูล MariaDB ไม่สามารถใช้ได้เมื่อฐานข้อมูลไม่ได้ทำงาน';
+$lang['mariadb_set_password'] = 'ตั้งรหัสผ่าน';
