c19804797ce894216118ef9d27af64e734627cbb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:19:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB məşhur verilənlər bazası sistemidir. Bu relational storage xidmət tələb verilənlər bazası idarə applications, web saytları, CRM və praktiki hər hansı digər resurs çalıştırmak üçün konfiqurasiya edilə bilər.';
+$lang['mariadb_app_tooltip'] = 'verilənlər bazası istifadə kök hesab sistem tərəfindən istifadə bir çox fərqli!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Giriş rədd edildi.';
+$lang['mariadb_password_updated'] = 'Parol uğurla yeniləndi.';
+$lang['mariadb_username_invalid'] = 'İstifadəçi adı yanlışdır.';
+$lang['mariadb_hostname_invalid'] = 'Hostname yanlışdır.';
+$lang['mariadb_password_invalid'] = 'Password yanlışdır.';
+$lang['mariadb_password_mismatch'] = 'Parol və uyğun gəlmir yoxlamaq.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Bir verilənlər bazası parol təyin edin.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'MariaDB idarə vasitədir daxil olmaq üçün linkə baxın. Siz verilənlər bazası parol dəyişdirmək istəyirsinizsə, aşağıdakı formu istifadə edin.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB Database Management Tool get';
+$lang['mariadb_management_tool_not_accessible'] = 'Verilənlər bazası çalışan zaman MariaDB Database Management Tool mövcud deyil';
+$lang['mariadb_set_password'] = 'Set Password';
