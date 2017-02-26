2c93d77826d9f39dabe8ff7fda5cb173c7815dae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:27:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Server bază de date';
+$lang['mariadb_app_description'] = 'MariaDB este un sistem de baze de date. Acesta poate fi configurat pentru a rula aplicații cu baze de date, site-uri web, CRM și, practic, orice altă resursă care necesită un serviciu de stocare relațională.';
+$lang['mariadb_app_tooltip'] = 'Contul de root utilizat de către baza de date este diferită de cea folosită de sistem!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Baza de date';
+$lang['mariadb_access_denied'] = 'Accesul este interzis.';
+$lang['mariadb_password_updated'] = 'Parolă actualizat cu succes.';
+$lang['mariadb_username_invalid'] = 'Numele de utilizator nu este valid.';
+$lang['mariadb_hostname_invalid'] = 'Este nevalid HOST-ului.';
+$lang['mariadb_password_invalid'] = 'Parolă este nevalid.';
+$lang['mariadb_password_mismatch'] = 'Parola și verificați nu se potrivesc.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Vă rugăm să setați o parolă bază de date.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB de stocare';
+$lang['mariadb_management_tool'] = 'Instrumentul de gestionare a MariaDB';
+$lang['mariadb_management_tool_help'] = 'Urmați link-ul pentru a accesa instrumentul de management al MariaDB. Dacă aveți nevoie să schimbați parola bazei de date, vă rugăm să folosiți formularul de mai jos.';
+$lang['mariadb_go_to_management_tool'] = 'Du-te la MariaDB Management Tool Baza de date';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB Instrumentul pentru managementul bazelor de date nu este disponibilă atunci când baza de date nu se execută';
+$lang['mariadb_set_password'] = 'Seteaza parola';
