75afff9bfbcbb13c760b12ed3ccb7b4e7de2b9b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:24:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Server ຖານຂໍ້ມູນ';
+$lang['mariadb_app_description'] = 'MariaDB ເປັນລະບົບຖານຂໍ້ມູນທີ່ນິຍົມ. ມັນກໍສາມາດໄດ້ຮັບການ configured ການດໍາເນີນການຄໍາຮ້ອງສະຫມັກຖານຂໍ້ມູນຂັບເຄື່ອນ, ເວັບໄຊທ໌, CRM ແລະການປະຕິບັດຊັບພະຍາກອນອື່ນໆທີ່ຕ້ອງການການບໍລິການການເກັບຮັກສາທີ່ກ່ຽວຂ້ອງ.';
+$lang['mariadb_app_tooltip'] = 'ບັນຊີຮາກໃຊ້ຖານຂໍ້ມູນທີ່ແຕກຕ່າງກັນກ່ວາຫນຶ່ງທີ່ນໍາໃຊ້ລະບົບໄດ້!';
+$lang['mariadb_mariadb_database'] = 'ຖານຂໍ້ມູນ MariaDB';
+$lang['mariadb_access_denied'] = 'ການເຂົ້າເຖິງຖືກປະຕິເສດ.';
+$lang['mariadb_password_updated'] = 'ລະຫັດຜ່ານການປັບປຸງຢ່າງສໍາເລັດຜົນ.';
+$lang['mariadb_username_invalid'] = 'ຊື່ຜູ້ໃຊ້ທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['mariadb_hostname_invalid'] = 'ທີ່ຢູ່ທີ່ຢູ່ທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['mariadb_password_invalid'] = 'ລະຫັດຜ່ານບໍ່ຖືກຕ້ອງ.';
+$lang['mariadb_password_mismatch'] = 'ລະຫັດຜ່ານແລະຢືນຢັນບໍ່ກົງກັນ.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ກະລຸນາຕັ້ງລະຫັດຜ່ານຖານຂໍ້ມູນ.';
+$lang['mariadb_mariadb_storage'] = 'ການເກັບຮັກສາ MariaDB';
+$lang['mariadb_management_tool'] = 'ເຄື່ອງມືການຈັດການ MariaDB';
+$lang['mariadb_management_tool_help'] = 'ປະຕິບັດຕາມການເຊື່ອມຕໍ່ໃນການເຂົ້າເຖິງເຄື່ອງມືການຈັດການ MariaDB. ຖ້າຫາກວ່າທ່ານຕ້ອງການທີ່ຈະມີການປ່ຽນແປງລະຫັດຜ່ານຖານຂໍ້ມູນຂອງທ່ານ, ກະລຸນາໃຊ້ແບບຟອມລຸ່ມນີ້.';
+$lang['mariadb_go_to_management_tool'] = 'ໄປ MariaDB ເຄື່ອງມືຈັດການຖານຂໍ້ມູນ';
+$lang['mariadb_management_tool_not_accessible'] = 'ເຄື່ອງມືການຈັດການຖານຂໍ້ມູນ MariaDB ບໍ່ສາມາດໃຊ້ໃນເວລາທີ່ຖານຂໍ້ມູນບໍ່ໄດ້ເຮັດວຽກ';
+$lang['mariadb_set_password'] = 'ກໍານົດລະຫັດຜ່ານ';
