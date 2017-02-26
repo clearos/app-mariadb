146368c9e51dd4c5895b1bd2ca32ac0fe41a7e31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:25:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Server база на податоци';
+$lang['mariadb_app_description'] = 'MariaDB е популарна база на податоци систем. Тоа може да биде конфигуриран да работи база на податоци управувано апликации, веб-сајтови, CRM и практично било кој друг ресурс бараат релациона складирање услуга.';
+$lang['mariadb_app_tooltip'] = 'Сметката root се користи од страна на базата на податоци е различен од оној што се користи од страна на системот!';
+$lang['mariadb_mariadb_database'] = 'MariaDB База на податоци';
+$lang['mariadb_access_denied'] = 'Забранет пристап.';
+$lang['mariadb_password_updated'] = 'Лозинка успешно ажурирани.';
+$lang['mariadb_username_invalid'] = 'Корисничко име не е валиден.';
+$lang['mariadb_hostname_invalid'] = 'Име на компјутерот не е валиден.';
+$lang['mariadb_password_invalid'] = 'Лозинка е погрешна.';
+$lang['mariadb_password_mismatch'] = 'Лозинка и потврди не си одговараат.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Ве молиме да поставите лозинка за базата на податоци.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB чување';
+$lang['mariadb_management_tool'] = 'Алатка за управување MariaDB';
+$lang['mariadb_management_tool_help'] = 'Следете го линкот за да пристапите до алатката за управување MariaDB. Ако треба да го смените вашата лозинка за базата на податоци, Ве молиме да користите формата подолу.';
+$lang['mariadb_go_to_management_tool'] = 'Оди на MariaDB алатката за управување со база на податоци';
+$lang['mariadb_management_tool_not_accessible'] = 'Алатката за управување со база на податоци MariaDB не е достапна кога базата на податоци не се извршува';
+$lang['mariadb_set_password'] = 'Поставете лозинка';
