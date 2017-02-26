dffad800218cb40473e5580032ee96463f9abaa3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:19:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB ডাটাবেস সার্ভার';
+$lang['mariadb_app_description'] = 'MariaDB একটি জনপ্রিয় ডাটাবেজ সিস্টেম. এটা ডাটাবেসের চালিত অ্যাপ্লিকেশন, ওয়েবসাইট, সিআরএম এবং কার্যত কোন অন্যান্য রিসোর্স একটি রিলেশনাল স্টোরেজ সেবা প্রয়োজন চালানোর জন্য কনফিগার করা যাবে.';
+$lang['mariadb_app_tooltip'] = 'রুট ডাটাবেসের দ্বারা ব্যবহৃত অ্যাকাউন্টের সিস্টেম দ্বারা ব্যবহৃত চেয়ে ভিন্ন!';
+$lang['mariadb_mariadb_database'] = 'MariaDB ডাটাবেস';
+$lang['mariadb_access_denied'] = 'অধিকার বাতিল হল.';
+$lang['mariadb_password_updated'] = 'পাসওয়ার্ড সফলভাবে আপডেট করা হয়েছে.';
+$lang['mariadb_username_invalid'] = 'ব্যবহারকারীর নাম অবৈধ.';
+$lang['mariadb_hostname_invalid'] = 'হোস্টনাম অবৈধ.';
+$lang['mariadb_password_invalid'] = 'পাসওয়ার্ড অবৈধ.';
+$lang['mariadb_password_mismatch'] = 'পাসওয়ার্ড এবং যাচাই মিলছে না.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'একটি ডাটাবেসের পাসওয়ার্ড সেট করুন.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB সংগ্রহস্থল';
+$lang['mariadb_management_tool'] = 'MariaDB ম্যানেজমেন্ট টুল';
+$lang['mariadb_management_tool_help'] = 'MariaDB ম্যানেজমেন্ট টুল অ্যাক্সেস লিঙ্কটি অনুসরণ করুন. আপনি আপনার ডাটাবেস পাসওয়ার্ড পরিবর্তন করার প্রয়োজন হলে, নিচের ফর্ম ব্যবহার করুন.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB ডাটাবেজ ম্যানেজমেন্ট টুল যান';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB ডাটাবেজ ম্যানেজমেন্ট টুল পাওয়া যায় না যখন ডাটাবেসের চলছে না';
+$lang['mariadb_set_password'] = 'Set Password এ';
