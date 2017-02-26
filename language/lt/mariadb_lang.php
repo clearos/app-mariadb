e8449d63e1cad633a8cf518471864694b35da682
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:24:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB duomenų bazių serveris';
+$lang['mariadb_app_description'] = 'MariaDB yra populiarus duomenų sistema. Jis gali būti sukonfigūruotas duomenų bazių, programų, interneto svetaines, CRM ir praktiškai bet kokią kitą išteklių reikalaujančių reliacinės saugojimo paslaugos.';
+$lang['mariadb_app_tooltip'] = 'Šaknis naudojamus apskaitos duomenų bazėje yra kitoks, nei pavaizduoto naudojamas sistemoje!';
+$lang['mariadb_mariadb_database'] = 'MariaDB duomenų bazė';
+$lang['mariadb_access_denied'] = 'Priėjimas negalimas.';
+$lang['mariadb_password_updated'] = 'Slaptažodis sėkmingai atnaujintas.';
+$lang['mariadb_username_invalid'] = 'Nick negalioja.';
+$lang['mariadb_hostname_invalid'] = 'Hostname yra neteisinga.';
+$lang['mariadb_password_invalid'] = 'Slaptažodis neteisingas.';
+$lang['mariadb_password_mismatch'] = 'Slaptažodis ir patikrinti nesutampa.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Prašome nustatyti duomenų bazės slaptažodį.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Sandėliavimo';
+$lang['mariadb_management_tool'] = 'MariaDB valdymo įrankis';
+$lang['mariadb_management_tool_help'] = 'Sekite nuorodą prieiti prie MariaDB valdymo įrankį. Jei jums reikia pakeisti savo duomenų bazės slaptažodį, naudokite žemiau esančią formą.';
+$lang['mariadb_go_to_management_tool'] = 'Eiti į MariaDB duomenų bazių valdymo įrankis';
+$lang['mariadb_management_tool_not_accessible'] = 'MariaDB Duomenų bazių valdymo įrankis neprieinamas kai duomenų bazė neveikia';
+$lang['mariadb_set_password'] = 'Nustatyti slaptažodį';
