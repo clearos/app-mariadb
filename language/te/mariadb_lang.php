472ecb37e6364b3b12e86fdbe10b9fd9ffda4664
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:29:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB డేటాబేస్ సర్వర్';
+$lang['mariadb_app_description'] = 'MariaDB ఒక ప్రముఖ డేటాబేస్ వ్యవస్థ. ఇది డేటాబేస్ నడుపబడే అప్లికేషన్లు, వెబ్సైట్లు, CRM మరియు ఆచరణాత్మకంగా ఏ యితర వనరు ఒక రిలేషనల్ నిల్వ సేవ అవసరం అమలు అమర్చవచ్చు.';
+$lang['mariadb_app_tooltip'] = 'డేటాబేస్ ద్వారా ఉపయోగిస్తారు root ఖాతా వ్యవస్థ ద్వారా ఉపయోగించే దాని కంటే భిన్నంగా ఉంటుంది!';
+$lang['mariadb_mariadb_database'] = 'MariaDB డేటాబేస్';
+$lang['mariadb_access_denied'] = 'అనుమతి తిరస్కరించబడింది.';
+$lang['mariadb_password_updated'] = 'పాస్వర్డ్ విజయవంతంగా నవీకరించబడింది.';
+$lang['mariadb_username_invalid'] = 'యూజర్ పేరు చెల్లనిది.';
+$lang['mariadb_hostname_invalid'] = 'పేరు చెల్లనిది.';
+$lang['mariadb_password_invalid'] = 'పాస్వర్డ్ చెల్లదు.';
+$lang['mariadb_password_mismatch'] = 'పాస్వర్డ్ మరియు ధ్రువీకరించడం సరిపోలడం లేదు.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'దయచేసి పాస్వర్డ్ను సెట్.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB నిల్వ';
+$lang['mariadb_management_tool'] = 'MariaDB మేనేజ్మెంట్ టూల్';
+$lang['mariadb_management_tool_help'] = 'MariaDB నిర్వహణ సాధనం ప్రాప్తి చేయడానికి లింక్ను అనుసరించండి. మీరు మీ పాస్వర్డ్ను మార్చడానికి ఉంటే, దయచేసి క్రింది ఫారమ్ ఉపయోగించండి.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB డేటాబేస్ మేనేజ్మెంట్ టూల్ వెళ్లు';
+$lang['mariadb_management_tool_not_accessible'] = 'డేటాబేస్ నడుస్తున్న లేనప్పుడు MariaDB డేటాబేస్ మేనేజ్మెంట్ టూల్ అందుబాటులో లేదు';
+$lang['mariadb_set_password'] = 'పాస్వర్డ్ సెట్';
