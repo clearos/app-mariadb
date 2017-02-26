1bb26514490e0cc13e945184c96489964394273f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:29:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB ay isang popular na database system. Maaari itong i-configure upang tumakbo database driven na mga aplikasyon, mga website, CRM at halos anumang iba pang mga mapagkukunan na nangangailangan ng isang pamanggit imbakan serbisyo.';
+$lang['mariadb_app_tooltip'] = 'Ang root account na ginagamit ng mga database ay naiiba kaysa sa isa na ginagamit ng sistema!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Access ay tinanggihan.';
+$lang['mariadb_password_updated'] = 'Password update matagumpay.';
+$lang['mariadb_username_invalid'] = 'Username ay hindi wasto.';
+$lang['mariadb_hostname_invalid'] = 'Hostname ay hindi wasto.';
+$lang['mariadb_password_invalid'] = 'Password ay hindi wasto.';
+$lang['mariadb_password_mismatch'] = 'Password at i-verify ay hindi tumutugma.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Mangyaring magtakda ng isang database password.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Sundin ang mga link upang ma-access ang management tool MariaDB. Kung kailangan mo upang baguhin ang iyong database password, mangyaring gamitin ang form sa ibaba.';
+$lang['mariadb_go_to_management_tool'] = 'Pumunta Upang MariaDB Database Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'Ang MariaDB Database Management Tool ay hindi magagamit kapag ang database ay hindi tumatakbo';
+$lang['mariadb_set_password'] = 'set Password';
