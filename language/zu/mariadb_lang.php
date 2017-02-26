62b4246c87cff2477e4a42de5b758d59168ea1d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:31:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB kuyinto ethandwayo uhlelo database. It kungenziwa amisiwe ukugijima database enophephela izicelo, amawebhusayithi, CRM futhi cishe noma yimiphi imithombo ezidinga indawo yokugcina zingubudlelwane.';
+$lang['mariadb_app_tooltip'] = 'I-akhawunti impande olusetshenziswa database ihlukile kunaleyo esetshenziswe uhlelo!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Ukufinyelela kunqatshelwe.';
+$lang['mariadb_password_updated'] = 'Iphasiwedi olusha ngempumelelo.';
+$lang['mariadb_username_invalid'] = 'Igama lomsebenzisi alivumelekile.';
+$lang['mariadb_hostname_invalid'] = 'Igama lomethuleli ayivumelekile.';
+$lang['mariadb_password_invalid'] = 'Iphasiwedi ayivumelekile.';
+$lang['mariadb_password_mismatch'] = 'Iphasiwedi futhi uqinisekise awafani.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Sicela usethe iphasiwedi database.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Isitoreji';
+$lang['mariadb_management_tool'] = 'Ithuluzi Management MariaDB';
+$lang['mariadb_management_tool_help'] = 'Landela isixhumanisi ukuze ufinyelele ukuphathwa ithuluzi MariaDB. Uma udinga ukushintsha iphasiwedi yakho egciniwe, sicela usebenzise ifomu elingezansi.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB Database Management Ithuluzi';
+$lang['mariadb_management_tool_not_accessible'] = 'I MariaDB Ithuluzi Database Management engatholakali lapho database is hhayi egijima';
+$lang['mariadb_set_password'] = 'Setha Iphasiwedi';
