fbc34b3d9635115f6287d772a093bb90b6775a22
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:23:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDBデータベースサーバー';
+$lang['mariadb_app_description'] = 'MariaDBは、人気のデータベースシステムです。 リレーショナルストレージサービスを必要とするデータベース駆動型アプリケーション、ウェブサイト、CRMおよび実質的に任意の他のリソースを実行するように設定することができます。';
+$lang['mariadb_app_tooltip'] = 'データベースで使用されるrootアカウントは、システムが使用しているものとは違います！';
+$lang['mariadb_mariadb_database'] = 'MariaDBデータベース';
+$lang['mariadb_access_denied'] = 'アクセスが拒否されました。';
+$lang['mariadb_password_updated'] = 'パスワードが正常に更新します。';
+$lang['mariadb_username_invalid'] = 'ユーザー名が無効です。';
+$lang['mariadb_hostname_invalid'] = 'ホスト名は無効です。';
+$lang['mariadb_password_invalid'] = 'パスワードが無効です。';
+$lang['mariadb_password_mismatch'] = 'パスワードと一致していないことを確認します。';
+$lang['mariadb_lang_please_set_a_database_password'] = 'データベースのパスワードを設定してください。';
+$lang['mariadb_mariadb_storage'] = 'MariaDBストレージ';
+$lang['mariadb_management_tool'] = 'MariaDB管理ツール';
+$lang['mariadb_management_tool_help'] = 'MariaDB管理ツールにアクセスするためのリンクをクリックしてください。 あなたは、データベースのパスワードを変更する必要がある場合は、下記のフォームをご利用ください。';
+$lang['mariadb_go_to_management_tool'] = 'MariaDBデータベース管理ツールにアクセスしてください';
+$lang['mariadb_management_tool_not_accessible'] = 'データベースが実行されていないときMariaDBデータベース管理ツールは利用できません';
+$lang['mariadb_set_password'] = 'パスワードを設定してください';
