a322632c5ab2a6277f0eeb89b862d071511bb26c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:24:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Дерекқор Сервер';
+$lang['mariadb_app_description'] = 'MariaDB танымал дерекқор жүйесі болып табылады. Ол өтініштер, веб-сайттар, CRM және атысты сақтау қызметі талап жүзінде кез келген басқа да ресурстарды жетегі дерекқорды іске қосу үшін теңшеуге болады.';
+$lang['mariadb_app_tooltip'] = 'деректер базасын пайдаланатын түбірлік тіркелгі жүйесі пайдаланатын бір қарағанда әр түрлі болып табылады!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Қатынас жоқ.';
+$lang['mariadb_password_updated'] = 'Құпия сөз сәтті жаңартылды.';
+$lang['mariadb_username_invalid'] = 'Пайдаланушы аты жарамсыз.';
+$lang['mariadb_hostname_invalid'] = 'Хост атауы жарамсыз болып табылады.';
+$lang['mariadb_password_invalid'] = 'Құпия сөз жарамсыз.';
+$lang['mariadb_password_mismatch'] = 'сәйкес келмейтін пароль және тексеру.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'дерекқор құпия сөзін орнатыңыз.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB сақтау';
+$lang['mariadb_management_tool'] = 'MariaDB басқару құралыName';
+$lang['mariadb_management_tool_help'] = 'MariaDB басқару құралын кіру үшін сілтеме бойынша өтіңіз. Егер сіз дерекқор құпия сөзін өзгерту қажет болса, төмендегі нысанды пайдаланыңыз.';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB Database Management құралына өтіңіз';
+$lang['mariadb_management_tool_not_accessible'] = 'дерекқор іске қосылмаған кезде MariaDB деректер базасымен басқару құралы қол жетімді емес';
+$lang['mariadb_set_password'] = 'орнату Құпия сөз';
