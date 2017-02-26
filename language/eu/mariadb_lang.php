22497215443d1f27d061e5e780520ac781ea8245
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:19:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database Server';
+$lang['mariadb_app_description'] = 'MariaDB popular datu-base sistema bat da. It konfiguratu daiteke datubase batetan aplikazioak, webguneak, CRM eta ia beste edozein baliabide biltegiratze zerbitzu erlazional bat eskatzen exekutatu.';
+$lang['mariadb_app_tooltip'] = 'root kontua datu-basean erabilitako sistema erabiltzen inork baino ezberdina da!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Database';
+$lang['mariadb_access_denied'] = 'Atzipena ukatu da.';
+$lang['mariadb_password_updated'] = 'Pasahitza ongi eguneratu da.';
+$lang['mariadb_username_invalid'] = 'Erabiltzaile izena baliogabea da.';
+$lang['mariadb_hostname_invalid'] = 'Hostname baliogabea da.';
+$lang['mariadb_password_invalid'] = 'Pasahitza ez da zuzena.';
+$lang['mariadb_password_mismatch'] = 'Pasahitza eta egiaztatu ez datoz bat.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Mesedez, datu-basearen pasahitza ezarri.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB kudeatzeko tresna';
+$lang['mariadb_management_tool_help'] = 'Jarraitu esteka MariaDB kudeaketa tresna sartzeko. Zure datu-basearen pasahitza aldatu behar izanez gero, mesedez, erabili beheko formularioa.';
+$lang['mariadb_go_to_management_tool'] = 'Joan MariaDB Database Management Erremintaren To';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB Database Management Tool ez da eskuragarri denean datu-basea ez dago martxan';
+$lang['mariadb_set_password'] = 'set Password';
