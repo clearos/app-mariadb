16aeaaa574086fece350a89d3786ce09437f91be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:28:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB K Server';
+$lang['mariadb_app_description'] = 'MariaDB ke e ratoa polokelongtshedimosetso tsamaiso. E ka di kentswe ho matha polokelongtshedimosetso khannoang dikopo, websites, CRM le mohlodi efe kapa efe e hoo e batlang e hlokang e le tšebeletso e relational polokelo.';
+$lang['mariadb_app_tooltip'] = 'Akhaonte motso sebelisoa ke polokelongtshedimosetso bo fapane ho feta ea sebelisoa ke tsamaiso!';
+$lang['mariadb_mariadb_database'] = 'MariaDB K';
+$lang['mariadb_access_denied'] = 'Access e latola.';
+$lang['mariadb_password_updated'] = 'Password ntjhafatswa ka katleho.';
+$lang['mariadb_username_invalid'] = 'Username ha o sebetse.';
+$lang['mariadb_hostname_invalid'] = 'Hostname ha o sebetse.';
+$lang['mariadb_password_invalid'] = 'Password ha o sebetse.';
+$lang['mariadb_password_mismatch'] = 'Password le netefatsa u se ke ua tšoanang le.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Ka kōpo beha k&#39;homphieutheng phasewete.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Storage';
+$lang['mariadb_management_tool'] = 'MariaDB Management Tool';
+$lang['mariadb_management_tool_help'] = 'Follow kgokahanyo ho fihlella MariaDB taolo ya sesebelisoa. Haeba u lokela ho fetola polokelongtshedimosetso phasewete ya hao, ka kōpo sebelisa foromong e ka tlaase.';
+$lang['mariadb_go_to_management_tool'] = 'Go Ho MariaDB K Management Tool';
+$lang['mariadb_management_tool_not_accessible'] = 'The MariaDB K Management Tool ke ha e fumanehe ha polokelongtshedimosetso e sa matha';
+$lang['mariadb_set_password'] = 'Set Password';
