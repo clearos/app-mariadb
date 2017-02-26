7a4696e8bcca3d1f0bb46c46dc1d75effbade1a5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:20:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Database ကို Server ကို';
+$lang['mariadb_app_description'] = 'MariaDB တစ်ဦးလူကြိုက်များဒေတာဘေ့စစနစ်ဖြစ်ပါသည်။ ဒါဟာ applications များ, ဝက်ဘ်ဆိုက်များ, CRM နှင့်တစ်ဦး relational သိုလှောင်မှုကိုဝန်ဆောင်မှုလိုအပ်သောလက်တွေ့မဆိုအခြားသယံဇာတမောင်းနှင်ဒေတာဘေ့စ run ဖို့ configured နိုင်ပါသည်။';
+$lang['mariadb_app_tooltip'] = 'ထိုဒေတာဘေ့စကအသုံးပြုအမြစ်အကောင့်စနစ်ကအသုံးပြုတထက်ကွဲပြားခြားနားသောပါ!';
+$lang['mariadb_mariadb_database'] = 'MariaDB ဒေတာဘေ့စ';
+$lang['mariadb_access_denied'] = 'ဝင်ရောက်ခွင့်ကိုငြင်းသည်။';
+$lang['mariadb_password_updated'] = 'Password ကိုအောင်မြင်စွာ update လုပ်။';
+$lang['mariadb_username_invalid'] = 'username မမှန်ကန်ဘူး။';
+$lang['mariadb_hostname_invalid'] = 'hostname မမှန်ကန်ဘူး။';
+$lang['mariadb_password_invalid'] = 'Password ကိုမမှန်ကန်ဘူး။';
+$lang['mariadb_password_mismatch'] = 'မတိုက်ဆိုင်ကြဘူး password နဲ့ verify ။';
+$lang['mariadb_lang_please_set_a_database_password'] = 'ဒေတာဘေ့စ password ကိုသတ်မှတ်ထားပေးပါ။';
+$lang['mariadb_mariadb_storage'] = 'MariaDB သိုလှောင်';
+$lang['mariadb_management_tool'] = 'MariaDB စီမံခန့်ခွဲမှု Tool ကို';
+$lang['mariadb_management_tool_help'] = 'အ MariaDB စီမံခန့်ခွဲမှု tool ကိုဝင်ရောက်ဖို့ link ကိုလိုက်နာပါ။ သင်သည်သင်၏ဒေတာဘေ့စ password ကိုပြောင်းလဲပစ်ရန်လိုလျှင်အောက်ပါပုံစံကိုသုံးပါ။';
+$lang['mariadb_go_to_management_tool'] = 'MariaDB Database ကိုစီမံခန့်ခွဲမှု Tool ကိုရန်ကိုသွားပါ';
+$lang['mariadb_management_tool_not_accessible'] = 'ထိုဒေတာဘေ့စ run မရသောအခါအဆိုပါ MariaDB Database ကိုစီမံခန့်ခွဲမှု Tool ကိုမရရှိနိုင်';
+$lang['mariadb_set_password'] = 'Set Password ကို';
