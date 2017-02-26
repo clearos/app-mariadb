d0b5b78da10b715e1ba200351ca7198a14447902
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:21:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/mariadb_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mariadb_app_name'] = 'MariaDB Datumbazo Servilo';
+$lang['mariadb_app_description'] = 'MariaDB estas populara datumbazo sistemo. Ĝi povas esti agordita por kuri datumbazo pelita aplikoj, retejoj, CRM kaj preskaŭ ajna alia rimedo postulanta rilata stokado servon.';
+$lang['mariadb_app_tooltip'] = 'La radiko konto uzita de la datumbazo estas malsama ol tiu uzita de la sistemo!';
+$lang['mariadb_mariadb_database'] = 'MariaDB Datumbazo';
+$lang['mariadb_access_denied'] = 'Aliro estas neita.';
+$lang['mariadb_password_updated'] = 'Pasvorto sukcese ĝisdatigita.';
+$lang['mariadb_username_invalid'] = 'Uzantonomo estas nevalida.';
+$lang['mariadb_hostname_invalid'] = 'Komputilnomo estas nevalida.';
+$lang['mariadb_password_invalid'] = 'Pasvorto estas nevalida.';
+$lang['mariadb_password_mismatch'] = 'Pasvorto kaj kontroli ne egalas.';
+$lang['mariadb_lang_please_set_a_database_password'] = 'Bonvolu agordi datumbazo pasvorton.';
+$lang['mariadb_mariadb_storage'] = 'MariaDB Stokado';
+$lang['mariadb_management_tool'] = 'MariaDB Management Ilo';
+$lang['mariadb_management_tool_help'] = 'Sekvu la ligon por aliri la MariaDB mastrumado ilo. Se vi bezonas ŝanĝi vian datumbazon pasvorton, bonvolu uzi la formon sub.';
+$lang['mariadb_go_to_management_tool'] = 'Go To MariaDB Datumbazo Management Ilo';
+$lang['mariadb_management_tool_not_accessible'] = 'La MariaDB Datumbazo Management Tool ne disponebla kiam la datumbazo ne estas kuranta';
+$lang['mariadb_set_password'] = 'aro Pasvorto';
